bf782b49323e869b0c7315cd95e3a32116bf94ca
al-imam <alimam01828@gmail.com>
2023-01-05 22:51:09 +0600

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <div>hello world</div>;
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,91 @@
+import classes from "./navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={classes.navbar}>
+      <ul className={classes.navbarNav}>
+        <li className={classes.navItem + " " + classes.active}>
+          <a href="#">Home</a>
+        </li>
+
+        <li className={classes.navItem}>
+          <a href="#">Department</a>
+          <ul className={classes.dropdownMenu}>
+            <li>
+              <a href="#">Computer Technology</a>
+            </li>
+            <li>
+              <a href="#">Electrical Technology</a>
+            </li>
+            <li>
+              <a href="#">Civil Technology</a>
+            </li>
+            <li>
+              <a href="#">Electronics Technology</a>
+            </li>
+            <li>
+              <a href="#">Architecture Technology</a>
+            </li>
+          </ul>
+        </li>
+
+        <li className={classes.navItem}>
+          <a href="#">Syllabus</a>
+          <ul className={classes.dropdownMenu}>
+            <li>
+              <a href="#">Computer Engineering Syllabus</a>
+            </li>
+            <li>
+              <a href="#">Civil Engineering Syllabus</a>
+            </li>
+            <li>
+              <a href="#">Electrical Engineering Syllabus</a>
+            </li>
+            <li>
+              <a href="#">Architecture Engineering Syllabus</a>
+            </li>
+          </ul>
+        </li>
+
+        <li className={classes.navItem}>
+          <a href="#">Facilities</a>
+          <ul className={classes.dropdownMenu}>
+            <li>
+              <a href="#">Modern Lab</a>
+            </li>
+            <li>
+              <a href="#">Open Library</a>
+            </li>
+            <li>
+              <a href="#">DUET Admission</a>
+            </li>
+            <li>
+              <a href="#">Free Internet</a>
+            </li>
+          </ul>
+        </li>
+
+        <li className={classes.navItem}>
+          <a href="#">LPI Clubs</a>
+          <ul className={classes.dropdownMenu}>
+            <li>
+              <a href="#">Robotic Club</a>
+            </li>
+            <li>
+              <a href="#">LPI programming club</a>
+            </li>
+            <li>
+              <a href="#">LPDS Debating Club</a>
+            </li>
+          </ul>
+        </li>
+
+        <li className={classes.navItem}>
+          <a href="#">About us</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;

65aafc9d657c2e3b7863417e2a8af5e0dc0f0908
al-imam <alimam01828@gmail.com>
2023-01-07 15:38:41 +0600

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Head from "./components/Head";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Head />;
+  return (
+    <BrowserRouter>
+      <Head />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

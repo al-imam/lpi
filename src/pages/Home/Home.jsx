32a60663081b99b1ba70fcbf76cfb478c83e2616
al-imam <alimam01828@gmail.com>
2023-01-09 02:24:22 +0600

    done hero section i may do some little changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,27 @@
 import classes from "./home.module.css";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 function Home() {
   return (
-    <main>
+    <main className={classes.main}>
+      <img
+        className={classes.backgroundImg}
+        src="/assets/collage.jpg"
+        alt="collage"
+      />
       <div className={classes.heroContainer}>
-        <img
-          className={classes.backgroundImg}
-          src="/assets/collage.jpg"
-          alt="collage"
-        />
         <div className={classes.hero}>
           <h1 className={classes.heading}>
-            Welcome to Lakshmipur polytechnic Institute
+            Welcome to Lakshmipur Polytechnic Institute
           </h1>
           <p className={classes.subtitle}>
             The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was
             established in 2006. It is located near Lakshmipur Stadium
             baishmara. It is under BTEB. It contains about 5,000 students.
           </p>
+          <a className={classes.action} href="/about">
+            Learn more <ArrowForwardIcon />
+          </a>
         </div>
       </div>
     </main>

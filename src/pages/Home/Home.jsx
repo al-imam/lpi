7fdb54da02f35296aa8c86c75178a57b1fbd2b45
al-imam <alimam01828@gmail.com>
2023-01-09 02:34:17 +0600

    separate text form component
      - create data.js file
      - remove all text from component
      - create object for text and export it default

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import classes from "./home.module.css";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { NavLink } from "react-router-dom";
+import data from "./data";
 
 function Home() {
   return (
@@ -12,16 +13,10 @@ function Home() {
       />
       <div className={classes.heroContainer}>
         <div className={classes.hero}>
-          <h1 className={classes.heading}>
-            Welcome to Lakshmipur Polytechnic Institute
-          </h1>
-          <p className={classes.subtitle}>
-            The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was
-            established in 2006. It is located near Lakshmipur Stadium
-            baishmara. It is under BTEB. It contains about 5,000 students.
-          </p>
+          <h1 className={classes.heading}>{data.title}</h1>
+          <p className={classes.subtitle}>{data.subtitle}</p>
           <NavLink className={classes.action} to="/about">
-            Learn more <ArrowForwardIcon />
+            {data.action} <ArrowForwardIcon />
           </NavLink>
         </div>
       </div>

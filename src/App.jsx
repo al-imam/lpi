6ac386c7a461666bc3cb0e5cf890bb5ca02ca55b
al-imam <alimam01828@gmail.com>
2023-01-08 15:12:47 +0600

    create page directory and Home page boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Head from "./components/Head";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Head from "./components/Head";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Head />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/complain" element={<h1>complain</h1>} />
       </Routes>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>Hello Home</div>;
+}
+
+export default Home;

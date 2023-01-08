c3f6a1da85f2f055df1db0554b2a5f7d845de5e3
al-imam <alimam01828@gmail.com>
2023-01-08 23:47:37 +0600

    style hero section
      - group title and subtitle
      - change font size and family

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,14 +9,16 @@ function Home() {
           src="/assets/collage.jpg"
           alt="collage"
         />
-        <h1 className={classes.heading}>
-          Welcome to Lakshmipur polytechnic Institute
-        </h1>
-        <p className={classes.subtitle}>
-          The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was
-          established in 2006. It is located near Lakshmipur Stadium baishmara.
-          It is under BTEB. It contains about 5,000 students.
-        </p>
+        <div className={classes.hero}>
+          <h1 className={classes.heading}>
+            Welcome to Lakshmipur polytechnic Institute
+          </h1>
+          <p className={classes.subtitle}>
+            The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was
+            established in 2006. It is located near Lakshmipur Stadium
+            baishmara. It is under BTEB. It contains about 5,000 students.
+          </p>
+        </div>
       </div>
     </main>
   );

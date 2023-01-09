e6d86701cb9db779e1da2ba39de9ea3808580377
al-imam <alimam01828@gmail.com>
2023-01-09 23:32:05 +0600

    add style to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,16 @@
+import classes from "./card.module.css";
+
 function Card() {
   return (
-    <div>
-      <img src="/assets/collage.jpg" alt="department picture" />
-      <div>
-        <h4>Computer science and technology (CST)</h4>
-        <div class="course-info">
+    <div className={classes.card}>
+      <img
+        className={classes.cardImg}
+        src="/assets/collage.jpg"
+        alt="department picture"
+      />
+      <div className={classes.cardInfo}>
+        <h4 className={classes.cardTitle}>Computer science and technology</h4>
+        <div class={classes.cardDetails}>
           <a href="#">Faculty of CST</a>
           <span>4 years</span>
         </div>

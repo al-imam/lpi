bf4096d379d1a24b125a7dfb39cd97d9c9d640b3
al-imam <alimam01828@gmail.com>
2023-01-09 22:26:55 +0600

    write card markup

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+function Card() {
+  return (
+    <div>
+      <img src="/assets/collage.jpg" alt="department picture" />
+      <div>
+        <h4>Computer science and technology (CST)</h4>
+        <div class="course-info">
+          <a href="#">Faculty of CST</a>
+          <span>4 years</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;

b96ddb92189deab94aac66d739fa1874388d16e7
al-imam <alimam01828@gmail.com>
2023-01-10 00:03:31 +0600

    Fix image hover with wrapper div

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,12 @@ import classes from "./card.module.css";
 function Card({ title, subtitle, src }) {
   return (
     <div className={classes.card}>
-      <img className={classes.cardImg} src={src} alt="department picture" />
+      <div className={classes.cardImgContainer}>
+        <img className={classes.cardImg} src={src} alt="department picture" />
+      </div>
       <div className={classes.cardInfo}>
         <h4 className={classes.cardTitle}>{title}</h4>
-        <div class={classes.cardDetails}>
+        <div className={classes.cardDetails}>
           <a href="#">{subtitle}</a>
           <span>4 years</span>
         </div>

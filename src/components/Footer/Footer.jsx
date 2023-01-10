aee4bc68fac4d7e913233783c829fb51b95b1e73
al-imam <alimam01828@gmail.com>
2023-01-11 01:21:34 +0600

    rearrange markup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,38 +12,6 @@ function Footer() {
     <footer className={classes.footerSection}>
       <div className={classes.container}>
         <ContactInfo />
-        <div className={classes.footerLogo}>
-          <a href="/" className={classes.logoImage}>
-            <img src="/assets/lpi.png" alt="logo" />
-          </a>
-          <p className={classes.logoDescription}>
-            The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was
-            established in 2006. It is located near Lakshmipur Stadium baishmara
-          </p>
-        </div>
-        <div className={classes.footerSocial}>
-          <h4 className={classes.socialHeading}>Follow us</h4>
-          <ul className={classes.socialItems}>
-            <li>
-              <a href="#" className={classes.socialItem}>
-                <GitHubIcon />
-                Github
-              </a>
-            </li>
-            <li>
-              <a href="#" className={classes.socialItem}>
-                <LinkedInIcon />
-                Linkedin
-              </a>
-            </li>
-            <li>
-              <a href="#" className={classes.socialItem}>
-                <FacebookIcon />
-                Facebook
-              </a>
-            </li>
-          </ul>
-        </div>
         <div className={classes.footerLinks}>
           <h4 className={classes.linkHeading}>
             Useful Links <LinkIcon />
@@ -66,6 +34,39 @@ function Footer() {
             </li>
             <li className={classes.linkItem}>
               <a href="#">LPI Clubs</a>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.footerLogo}>
+          <a href="/" className={classes.logoImage}>
+            <img src="/assets/lpi.png" alt="logo" />
+          </a>
+          <p className={classes.logoDescription}>
+            The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was
+            established in 2006. It is located near Lakshmipur Stadium baishmara
+          </p>
+        </div>
+
+        <div className={classes.footerSocial}>
+          <h4 className={classes.socialHeading}>Follow us</h4>
+          <ul className={classes.socialItems}>
+            <li>
+              <a href="#" className={classes.socialItem}>
+                <GitHubIcon />
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="#" className={classes.socialItem}>
+                <LinkedInIcon />
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a href="#" className={classes.socialItem}>
+                <FacebookIcon />
+                Facebook
+              </a>
             </li>
           </ul>
         </div>

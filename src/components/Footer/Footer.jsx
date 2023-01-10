da1311efcc4941a297e4a15ac0d49e0cd81ed61e
al-imam <alimam01828@gmail.com>
2023-01-10 22:14:52 +0600

    add contactInfo component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,4 @@
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import MailLockIcon from "@mui/icons-material/MailLock";
-import PhonelinkLockIcon from "@mui/icons-material/PhonelinkLock";
+import ContactInfo from "./ContactInfo/ContactInfo";
 
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -13,38 +11,7 @@ function Footer() {
   return (
     <footer className={classes.footerSection}>
       <div className={classes.container}>
-        <div>
-          <div className={classes.footerContact}>
-            <LocationOnIcon />
-            <div className={classes.contactInfo}>
-              <h4 className={classes.infoHeading}>Find us</h4>
-              <address className={classes.infoDescription}>
-                132554 Laharkandi, Lakshmipur, Chittagong
-              </address>
-            </div>
-          </div>
-          <div className={classes.footerContact}>
-            <PhonelinkLockIcon />
-            <div className={classes.contactInfo}>
-              <h4 className={classes.infoHeading}>Call us</h4>
-              <a className={classes.infoDescription} href="tel:+8801881614926">
-                +8801881614926
-              </a>
-            </div>
-          </div>
-          <div className={classes.footerContact}>
-            <MailLockIcon />
-            <div className={classes.contactInfo}>
-              <h4 className={classes.infoHeading}>Mail us</h4>
-              <a
-                className={classes.infoDescription}
-                href="mailto:alimam01828@gmail.com"
-              >
-                alimam01828@gmail.com
-              </a>
-            </div>
-          </div>
-        </div>
+        <ContactInfo />
         <div>
           <div>
             <a href="#">

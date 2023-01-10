2a0be63adf8b98a20fbf0b209711a611e92689ed
al-imam <alimam01828@gmail.com>
2023-01-10 22:13:28 +0600

    separate footer contact info on his own component

---
+++ src/components/Footer/ContactInfo/ContactInfo.jsx
@@ -0,0 +1,44 @@
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import MailLockIcon from "@mui/icons-material/MailLock";
+import PhonelinkLockIcon from "@mui/icons-material/PhonelinkLock";
+
+import classes from "./contactInfo.module.css";
+
+function ContactInfo() {
+  return (
+    <div>
+      <div className={classes.footerContact}>
+        <LocationOnIcon />
+        <div className={classes.contactInfo}>
+          <h4 className={classes.infoHeading}>Find us</h4>
+          <address className={classes.infoDescription}>
+            132554 Laharkandi, Lakshmipur, Chittagong
+          </address>
+        </div>
+      </div>
+      <div className={classes.footerContact}>
+        <PhonelinkLockIcon />
+        <div className={classes.contactInfo}>
+          <h4 className={classes.infoHeading}>Call us</h4>
+          <a className={classes.infoDescription} href="tel:+8801881614926">
+            +8801881614926
+          </a>
+        </div>
+      </div>
+      <div className={classes.footerContact}>
+        <MailLockIcon />
+        <div className={classes.contactInfo}>
+          <h4 className={classes.infoHeading}>Mail us</h4>
+          <a
+            className={classes.infoDescription}
+            href="mailto:alimam01828@gmail.com"
+          >
+            alimam01828@gmail.com
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactInfo;

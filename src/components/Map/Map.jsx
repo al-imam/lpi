e6e1fa918af73540675625d1ba583593faad4531
al-imam <alimam01828@gmail.com>
2023-01-12 00:13:20 +0600

    create Map component

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,9 @@
+export default function Map() {
+  return (
+    <iframe
+      width="100%"
+      height="512"
+      src="https://maps.google.com/maps?q=Lakshmipur%20Polytechnic%20Institute%20%E0%A6%B2%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%80%E0%A6%AA%E0%A7%81%E0%A6%B0%20%E0%A6%AA%E0%A6%B2%E0%A6%BF%E0%A6%9F%E0%A7%87%E0%A6%95%E0%A6%A8%E0%A6%BF%E0%A6%95%E2%80%A6&t=&z=17&ie=UTF8&iwloc=&output=embed"
+    />
+  );
+}

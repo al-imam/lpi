1a64816ebd8e211f37dc204ec09522da0f9285fd
al-imam <alimam01828@gmail.com>
2023-01-14 22:07:04 +0600

    change markup

--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -13,12 +13,9 @@ function Study() {
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which don't look even slightly believable.
-          If you are going to use a passage of Lorem Ipsum, you need to be sure
-          there isn't anything embarrassing hidden in the middle of text amr
-          songr balga ami toami valo lasi ciri din akr dali
         </p>
         <NavLink className={classes.link} to="/about">
-          Learn Now
+          Learn more
         </NavLink>
       </div>
     </div>

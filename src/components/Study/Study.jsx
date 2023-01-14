afa88d8a180e9accd45a62d02049499b4a97c5fe
al-imam <alimam01828@gmail.com>
2023-01-15 00:31:52 +0600

    wrapped with section and style section

--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -3,20 +3,23 @@ import { NavLink } from "react-router-dom";
 
 function Study() {
   return (
-    <div className={classes.studyContainer}>
-      <img className={classes.image} src="assets/collage.jpg" alt="collage" />
-      <div className={classes.info}>
-        <h3 className={classes.title}>About LPI</h3>
-        <p className={classes.subtitle}>
-          There are many variations of passages of Lorem Ipsum available, but
-          the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable.
-        </p>
-        <NavLink className={classes.link} to="/about">
-          Learn more
-        </NavLink>
+    <section className={classes.section}>
+      <div className={classes.studyContainer}>
+        <img className={classes.image} src="assets/collage.jpg" alt="collage" />
+        <div className={classes.info}>
+          <h3 className={classes.title}>About LPI</h3>
+          <p className={classes.subtitle}>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </p>
+          <NavLink className={classes.link} to="/about">
+            Learn more
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 

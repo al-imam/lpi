4f9fc638a9ab74c8a2d617c88284e8e0665ee777
al-imam <alimam01828@gmail.com>
2023-01-15 00:59:27 +0600

    move department related data to Departments/data.js file

---
+++ src/components/Departments/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: "Computer science and technology",
+    src: "/assets/collage.jpg",
+    subtitle: "Faculty of CST",
+  },
+  {
+    title: "Computer science and technology",
+    src: "/assets/collage.jpg",
+    subtitle: "Faculty of CST",
+  },
+  {
+    title: "Computer science and technology",
+    src: "/assets/collage.jpg",
+    subtitle: "Faculty of CST",
+  },
+  {
+    title: "Computer science and technology",
+    src: "/assets/collage.jpg",
+    subtitle: "Faculty of CST",
+  },
+  {
+    title: "Computer science and technology",
+    src: "/assets/collage.jpg",
+    subtitle: "Faculty of CST",
+  },
+];
--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -2,31 +2,4 @@ export default {
   title: "Welcome to Lakshmipur Polytechnic",
   subtitle: `The Lakshmipur Polytechnic Institute in Lakshmipur, Bangladesh, was established in 2006. It is located near Lakshmipur Stadium baishmara. It is under BTEB. It contains about 5,000 students.`,
   action: "Learn more",
-  cards: [
-    {
-      title: "Computer science and technology",
-      src: "/assets/collage.jpg",
-      subtitle: "Faculty of CST",
-    },
-    {
-      title: "Computer science and technology",
-      src: "/assets/collage.jpg",
-      subtitle: "Faculty of CST",
-    },
-    {
-      title: "Computer science and technology",
-      src: "/assets/collage.jpg",
-      subtitle: "Faculty of CST",
-    },
-    {
-      title: "Computer science and technology",
-      src: "/assets/collage.jpg",
-      subtitle: "Faculty of CST",
-    },
-    {
-      title: "Computer science and technology",
-      src: "/assets/collage.jpg",
-      subtitle: "Faculty of CST",
-    },
-  ],
 };

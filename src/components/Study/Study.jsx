58ac86b8f7f70ceb6754bdcfbf16dcf14bbb801e
al-imam <alimam01828@gmail.com>
2023-01-14 21:36:36 +0600

    create html structure

---
+++ src/components/Study/Study.jsx
@@ -0,0 +1,21 @@
+function Study() {
+  return (
+    <div>
+      <div>
+        <img src="assets/collage.jpg" alt="collage" />
+      </div>
+      <div>
+        <h3>About LPI</h3>
+        <p>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+          If you are going to use a passage of Lorem Ipsum, you need to be sure
+          there isn't anything embarrassing hidden in the middle of text amr
+          songr balga ami toami valo lasi ciri din akr dali
+        </p>
+        <a>Learn Now</a>
+      </div>
+    </div>
+  );
+}

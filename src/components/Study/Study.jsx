d973ad6576442822012b3d8960e8abf768510df9
al-imam <alimam01828@gmail.com>
2023-01-15 00:48:24 +0600

    create new data.js file store all text here

--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -1,19 +1,15 @@
 import classes from "./study.module.css";
 import { NavLink } from "react-router-dom";
+import data from "./data";
 
 function Study() {
   return (
     <section className={classes.section}>
       <div className={classes.studyContainer}>
-        <img className={classes.image} src="assets/collage.jpg" alt="collage" />
+        <img className={classes.image} src={data.src} alt="collage" />
         <div className={classes.info}>
           <h3 className={classes.title}>About LPI</h3>
-          <p className={classes.subtitle}>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable.
-          </p>
+          <p className={classes.subtitle}>{data.subtitle}</p>
           <NavLink className={classes.link} to="/about">
             Learn more
           </NavLink>

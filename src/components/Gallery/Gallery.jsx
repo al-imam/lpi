29968a74e9c94b4f943931f88bdd632672533b91
al-imam <alimam01828@gmail.com>
2023-01-16 02:49:49 +0600

    close model on outside click
      - best thing i ever done with css
        i don't know i do it

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,9 +4,9 @@ import useOutsideClick from "../../hooks/useOutSideClick";
 
 function Gallery({ pictures }) {
   return (
-    <div className="App">
-      {pictures.map(({ url }) => (
-        <Picture key={url + Math.random()} url={url} />
+    <div className={classes.gallery}>
+      {pictures.map(({ url }, index) => (
+        <Picture show={index} key={url + Math.random()} url={url} />
       ))}
     </div>
   );
@@ -18,18 +18,18 @@ function Picture({ url }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const onClick = useCallback(() => {
-    if (open) {
+    if (isOpen) {
       setIsOpen(false);
     }
-    console.log("im called", open);
-  }, [open]);
+    console.log("im called", isOpen);
+  }, [isOpen]);
 
   const ref = useOutsideClick(onClick);
 
   return (
     <div
       ref={ref}
-      className={isOpen ? classes.open : ""}
+      className={`${classes.imgContainer} ${isOpen ? classes.open : ""}`}
       onClick={() => setIsOpen(true)}
     >
       <img src={url} />

d04ca3cd16804490ff089c544c59bd2f62d11cd3
al-imam <alimam01828@gmail.com>
2023-01-15 22:22:38 +0600

    style text and make layout with flex box

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -3,21 +3,23 @@ import classes from "./status.module.css";
 function Status() {
   return (
     <section className={classes.status}>
-      <div>
-        <span>35</span>
-        <span>Teacher</span>
-      </div>
-      <div>
-        <span>12</span>
-        <span>Instructor</span>
-      </div>
-      <div>
-        <span>4000</span>
-        <span>Student</span>
-      </div>
-      <div>
-        <span>5</span>
-        <span>Department</span>
+      <div className={classes.container}>
+        <div className={classes.group}>
+          <span>35</span>
+          <span>Teacher</span>
+        </div>
+        <div className={classes.group}>
+          <span>12</span>
+          <span>Instructor</span>
+        </div>
+        <div className={classes.group}>
+          <span>4000</span>
+          <span>Student</span>
+        </div>
+        <div className={classes.group}>
+          <span>5</span>
+          <span>Department</span>
+        </div>
       </div>
     </section>
   );

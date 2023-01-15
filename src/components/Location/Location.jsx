ff53489d36cccc1315a78e40bd4d6cb3bf138a39
al-imam <alimam01828@gmail.com>
2023-01-15 21:14:22 +0600

    add svg icons

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,10 +1,13 @@
 import classes from "./location.module.css";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
-function Location() {
+function Location({ page = "About" }) {
   return (
     <div className={classes.container}>
       <div className={classes.flex}>
         <span>Home</span>
+        <NavigateNextIcon />
+        <span>{page}</span>
       </div>
     </div>
   );

d3b13ee25c3d3cf6d03b9b5d959b6cf471b2d9e5
al-imam <alimam01828@gmail.com>
2023-01-15 20:40:53 +0600

    add markup

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,7 +1,13 @@
 import classes from "./location.module.css";
 
 function Location() {
-  return <div className={classes.container}>Shittier</div>;
+  return (
+    <div className={classes.container}>
+      <div>
+        <span>Home</span>
+      </div>
+    </div>
+  );
 }
 
 export default Location;

81c4510758e61697312d4e5d2acaab36f7283e9b
al-imam <alimam01828@gmail.com>
2023-01-15 21:32:15 +0600

    optionally show sub routes

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,13 +1,19 @@
 import classes from "./location.module.css";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
-function Location({ page = "About" }) {
+function Location({ current = "About", subRoute = null }) {
   return (
     <div className={classes.container}>
       <div className={classes.flex}>
         <span>Home</span>
         <NavigateNextIcon />
-        <span>{page}</span>
+        <span>{current}</span>
+        {subRoute && (
+          <>
+            <NavigateNextIcon />
+            <span>{subRoute}</span>
+          </>
+        )}
       </div>
     </div>
   );

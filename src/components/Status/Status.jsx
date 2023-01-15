dcb92dbbd1274e373a5af93396f34113da4ef824
al-imam <alimam01828@gmail.com>
2023-01-15 22:02:19 +0600

    create html markup

---
+++ src/components/Status/Status.jsx
@@ -0,0 +1,22 @@
+function Status() {
+  return (
+    <div>
+      <div>
+        <span>35</span>
+        <span>Teacher</span>
+      </div>
+      <div>
+        <span>12</span>
+        <span>Instructor</span>
+      </div>
+      <div>
+        <span>4000</span>
+        <span>Student</span>
+      </div>
+      <div>
+        <span>5</span>
+        <span>Department</span>
+      </div>
+    </div>
+  );
+}

1ef809fb28f10b42f0b3166d92e32305a2be4fa2
al-imam <alimam01828@gmail.com>
2023-01-16 23:28:16 +0600

    Fix image load issue solve but how i don't know

--- src/pages/About/data.js
+++ src/pages/About/data.js
@@ -1,26 +1,26 @@
 export default [
   {
-    url: "/public/general/_ (1).JPG",
+    url: "/general/_ (1).JPG",
   },
   {
-    url: "/public/general/_ (2).JPG",
+    url: "/general/_ (2).JPG",
   },
   {
-    url: "/public/general/_ (7).JPG",
+    url: "/general/_ (7).JPG",
   },
   {
-    url: "/public/general/_ (3).JPG",
+    url: "/general/_ (3).JPG",
   },
   {
-    url: "/public/general/_ (8).JPG",
+    url: "/general/_ (8).JPG",
   },
   {
-    url: "/public/general/_ (4).JPG",
+    url: "/general/_ (4).JPG",
   },
   {
-    url: "/public/general/_ (5).JPG",
+    url: "/general/_ (5).JPG",
   },
   {
-    url: "/public/general/_ (6).JPG",
+    url: "/general/_ (6).JPG",
   },
 ];

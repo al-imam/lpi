2288c85f2e94c6d5f01367ee3b28575f83411ad3
al-imam <alimam01828@gmail.com>
2023-01-17 01:37:31 +0600

    Split code

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,12 @@
+import { lazy, Suspense } from "react";
 import classes from "./about.module.css";
 import Location from "../../components/Location/Location";
 import Study from "../../components/Study/Study";
 import Status from "../../components/Status/Status";
-import Gallery from "../../components/Gallery/Gallery";
+// import Gallery from "../../components/Gallery/Gallery";
 import data from "./data";
+
+const Gallery = lazy(() => import("../../components/Gallery/Gallery"));
 
 function About() {
   return (
@@ -13,7 +16,9 @@ function About() {
         <Study subtitle={data.about} />
       </main>
       <Status />
-      <Gallery pictures={data.gallery} heading="Image Gallery" />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Gallery pictures={data.gallery} heading="Image Gallery" />
+      </Suspense>
     </div>
   );
 }

1e130d22ed0f3d8bb30169cc76b69c3e83305642
al-imam <alimam01828@gmail.com>
2023-01-17 01:27:11 +0600

    remove react lazy and  suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
 import Head from "./components/Head/Head";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
-
-const Home = lazy(() => import("./pages/Home/Home"));
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Head />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<h1>Loading....</h1>}>
-              <Home />
-            </Suspense>
-          }
-        />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/complain" element={<h1>complain</h1>} />
       </Routes>

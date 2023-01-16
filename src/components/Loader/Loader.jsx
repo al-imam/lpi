09771eb58d53b01233c94ab7d101a64c80618cbd
al-imam <alimam01828@gmail.com>
2023-01-17 01:49:40 +0600

    create loader markup

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import classes from "./loader.module.css";
+
+function Loader() {
+  return (
+    <div className={classes.container}>
+      <div className={classes.loadContainer}>
+        <div className={classes.lineSpinner}></div>
+      </div>
+    </div>
+  );
+}

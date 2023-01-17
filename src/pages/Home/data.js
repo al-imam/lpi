c6b602bb1f3960c2dd18ffc39c0ed7edd4978faa
al-imam <alimam01828@gmail.com>
2023-01-17 14:20:28 +0600

    add image data to home component

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -1,3 +1,29 @@
 export default {
   about: `Lakshmipur polytechnic institute was established in 2006. This institute conducts 4 years Diploma in Engineering course under bangladesh Technical education board. In addition to technical education every technology has a non-technical education department for teaching compulsory subjects such as Bangla, English, Mathematics, Physics, Chemistry, Social Science etc to the students. students are taught in two shifts morning and afternoon.`,
+  gallery: [
+    {
+      url: "/general/_ (1).JPG",
+    },
+    {
+      url: "/general/_ (2).JPG",
+    },
+    {
+      url: "/general/_ (7).jpg",
+    },
+    {
+      url: "/general/_ (3).JPG",
+    },
+    {
+      url: "/general/_ (8).jpg",
+    },
+    {
+      url: "/general/_ (4).JPG",
+    },
+    {
+      url: "/general/_ (5).jpg",
+    },
+    {
+      url: "/general/_ (6).jpg",
+    },
+  ],
 };

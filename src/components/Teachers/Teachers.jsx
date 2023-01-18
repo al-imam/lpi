eeca450ee8fef49d008104c0e2325ebd5c6eae7d
al-imam <alimam01828@gmail.com>
2023-01-18 18:07:27 +0600

    create teacher section structure

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -7,15 +7,33 @@ function Teachers() {
       <div className={classes.container}>
         <h2>Teachers</h2>
         <div className={classes.teachers}>
-          <Teacher />
-          <Teacher />
-          <Teacher />
-          <Teacher />
-          <Teacher />
-          <Teacher />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
     </section>
+  );
+}
+
+function Card() {
+  return (
+    <div className={classes.card}>
+      <img
+        srcSet=""
+        style={{ maxWidth: "100%" }}
+        src="/sir/maruf-sir.JPG"
+        alt=""
+      />
+      <p className={classes.info}>
+        <h4>maruf hussein</h4>
+        <span>naishmara, lakshmipur</span>
+      </p>
+    </div>
   );
 }
 

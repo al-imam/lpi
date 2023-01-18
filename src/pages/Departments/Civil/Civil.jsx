fc50e5a38c69c5841303a38a6bae74b387cd9b10
al-imam <alimam01828@gmail.com>
2023-01-19 01:10:07 +0600

    lazy load TP component

--- src/pages/Departments/Civil/Civil.jsx
+++ src/pages/Departments/Civil/Civil.jsx
@@ -1,8 +1,15 @@
-import TP from "../../../components/TP/TP";
+import { lazy, Suspense } from "react";
+import Loader from "../../../components/Loader/Loader";
 import data from "./data";
 
+const TP = lazy(() => import("../../../components/TP/TP"));
+
 function Civil() {
-  return <TP heading={data.heading} teacher={data.teacher} />;
+  return (
+    <Suspense fallback={<Loader />}>
+      <TP heading={data.heading} teacher={data.teacher} />
+    </Suspense>
+  );
 }
 
 export default Civil;

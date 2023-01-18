1d6c1a8468b896241e7f364c736d4f48a93b901f
al-imam <alimam01828@gmail.com>
2023-01-19 01:08:47 +0600

    remove lazy loading from department pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Head from "./components/Head/Head";
 import Footer from "./components/Footer/Footer";
-const About = lazy(() => import("./pages/About/About"));
 import Home from "./pages/Home/Home";
 import Departments from "./pages/Departments/Departments";
 import Loader from "./components/Loader/Loader";
-const Computer = lazy(() => import("./pages/Departments/Computer/Computer"));
-const Civil = lazy(() => import("./pages/Departments/Civil/Civil"));
+import Computer from "./pages/Departments/Computer/Computer";
+import Civil from "./pages/Departments/Civil/Civil";
+const About = lazy(() => import("./pages/About/About"));
 
 function App() {
   return (
@@ -24,22 +24,8 @@ function App() {
           }
         />
         <Route path="/departments" element={<Departments />} />
-        <Route
-          path="/departments/computer-technology"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Computer />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/departments/civil-technology"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Civil />
-            </Suspense>
-          }
-        />
+        <Route path="/departments/computer-technology" element={<Computer />} />
+        <Route path="/departments/civil-technology" element={<Civil />} />
         <Route
           path="/departments/electrical-technology"
           element={<h1>Electrical</h1>}

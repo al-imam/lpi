1e38819678cf713023b14a8066fc66597cf5c7dc
al-imam <alimam01828@gmail.com>
2023-01-19 00:54:00 +0600

    create civil department page

---
+++ src/pages/Departments/Civil/Civil.jsx
@@ -0,0 +1,8 @@
+import TP from "../../../components/TP/TP";
+import data from "./data";
+
+function Civil() {
+  return <TP heading={data.heading} teacher={data.teacher} />;
+}
+
+export default Civil;
---
+++ src/pages/Departments/Civil/data.js
@@ -0,0 +1,116 @@
+export default {
+  heading: {
+    src: "/assets/collage.jpg",
+    description: `Civil engineering is a professional engineering discipline that deals with the design, construction, and maintenance of the physical and naturally built environment, including public works such as roads, bridges, canals, dams, airports, sewage systems, pipelines, structural components of buildings, and railway.
+    Civil engineering is traditionally broken into a number of sub-disciplines. It is considered the second-oldest engineering discipline after military engineering  and it is defined to distinguish non-military engineering from military engineering.
+    Civil engineering can take place in the public sector from municipal public works departments through to federal government agencies, and in the private sector from locally based firms to global Fortune 500 companies.
+    Civil engineering is the application of physical and scientific principles for solving the problems of society, and its history is intricately linked to advances in the understanding of physics and mathematics throughout history. Because civil engineering is a broad profession, including several specialized sub-disciplines, its history is linked to knowledge of structures, materials science, geography, geology, soils, hydrology, environmental science, mechanics, project management, and other fields.
+    Throughout ancient and medieval history most architectural design and construction was carried out by artisans, such as stonemasons and carpenters, rising to the role of master builder. Knowledge was retained in guilds and seldom supplanted by advances. Structures, roads, and infrastructure that existed were repetitive, and increases in scale were incremental.`,
+    name: "Civil Technology",
+    subpath: "Civil",
+    firstShift: {
+      src: "/sir/sazzadul.jpg",
+      name: "Sazzadul Hasan",
+      address: "Raipur, Lakshmipur ",
+      facebook: "https://www.facebook.com/profile.php?id=100009483072562",
+      linkedin: "#",
+    },
+    secondShift: {
+      src: "/sir/nasir-sir.JPG",
+      name: "Md Nasir Uddin",
+      address: "Feni Sadar, Feni",
+      facebook: "https://www.facebook.com/profile.php?id=100002424941884",
+      linkedin: "#",
+    },
+  },
+
+  teacher: [
+    {
+      name: "Md Musleh Uddin",
+      title: "Teacher Guest",
+      src: "/sir/musleh-sir.JPG",
+      facebook: "",
+      linkedin: "#",
+    },
+
+    {
+      name: "Md Mehraj Uddin",
+      title: "Craft Instructor",
+      src: "/sir/mehraj-sir.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Md Mehadi Hasan",
+      title: "Craft Instructor",
+      src: "/sir/mehadi.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Md Shamim Hossain",
+      title: "Craft Instructor",
+      src: "/sir/shamim.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+
+    {
+      name: "Sabekun Nahar",
+      title: "Craft Instructor",
+      src: "/sir/sabekun.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Halima Mehjabin",
+      title: "Craft Instructor",
+      src: "/sir/halima.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+
+    {
+      name: "Md Roki Hossain",
+      title: "Craft Instructor",
+      src: "/sir/Roki.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Rupa Moni",
+      title: "Craft Instructor",
+      src: "/sir/Rupa.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Mehadi Hasan",
+      title: "Craft Instructor",
+      src: "/sir/mehadi-1.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Reduan Hossain",
+      title: "Craft Instructor",
+      src: "/sir/reduan.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Nilupa Akther",
+      title: "Craft Instructor",
+      src: "/sir/Nilupa.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+    {
+      name: "Puja rani das",
+      title: "Teacher Guest",
+      src: "/sir/Nilupa.JPG",
+      facebook: "#",
+      linkedin: "#",
+    },
+  ],
+};

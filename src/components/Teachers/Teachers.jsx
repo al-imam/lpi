d7b5e7bd742ddea67f3617222f7abf571d268196
al-imam <alimam01828@gmail.com>
2023-01-18 20:19:06 +0600

    create teacher section

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,5 +1,6 @@
 import classes from "./teacher.module.css";
-import Teacher from "../Teacher/Teacher";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Teachers() {
   return (
@@ -23,16 +24,21 @@ function Teachers() {
 function Card() {
   return (
     <div className={classes.card}>
-      <img
-        srcSet=""
-        style={{ maxWidth: "100%" }}
-        src="/sir/maruf-sir.JPG"
-        alt=""
-      />
-      <p className={classes.info}>
-        <h4>maruf hussein</h4>
-        <span>naishmara, lakshmipur</span>
-      </p>
+      <img style={{ maxWidth: "100%" }} src="/sir/maruf-sir.JPG" alt="" />
+      <div className={classes.info}>
+        <p>
+          <h4>maruf hussein</h4>
+          <span>baishmara, lakshmipur</span>
+        </p>
+        <p className={classes.icons}>
+          <a href="#">
+            <FacebookIcon />
+          </a>
+          <a href="#">
+            <LinkedInIcon />
+          </a>
+        </p>
+      </div>
     </div>
   );
 }

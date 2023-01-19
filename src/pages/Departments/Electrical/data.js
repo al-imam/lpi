5d9c45d6887d40554ba5c2adaefb4ad0ffe6ecab
al-imam <alimam01828@gmail.com>
2023-01-20 02:32:20 +0600

    electrical data chnage

--- src/pages/Departments/Electrical/data.js
+++ src/pages/Departments/Electrical/data.js
@@ -1,23 +1,22 @@
 export default {
   heading: {
-    src: "/assets/collage.jpg",
+    src: "/Electrical/civil-2.JPG",
     description: `Electrical engineering is an engineering discipline concerned with the study, design, and application of equipment, devices, and systems which use electricity, electronics, and electromagnetism. It emerged as an identifiable occupation in the latter half of the 19th century after commercialization of the electric telegraph, the telephone, and electrical power generation, distribution, and use.
-
-    Electrical engineering is now divided into a wide range of different fields, including computer engineering, systems engineering, power engineering, telecommunications, radio-frequency engineering, signal processing, instrumentation, photovoltaic cells, electronics, and optics and photonics. Many of these disciplines overlap with other engineering branches, spanning a huge number of specializations including hardware engineering, power electronics, electromagnetics and waves, microwave engineering, nanotechnology, electrochemistry, renewable energies, mechatronics/control, and electrical materials science.
-    Electrical engineers typically hold a degree in electrical engineering or electronic engineering. Practising engineers may have professional certification and be members of a professional body or an international standards organization. These include the International Electrotechnical Commission (IEC), the Institute of Electrical and Electronics Engineers (IEEE) and the Institution of Engineering and Technology `,
+      Electrical engineering is now divided into a wide range of different fields, including computer engineering, systems engineering, power engineering, telecommunications, radio-frequency engineering, signal processing, instrumentation, photovoltaic cells, electronics, and optics and photonics. Many of these disciplines overlap with other engineering branches, spanning a huge number of specializations including hardware engineering, power electronics, electromagnetics and waves, microwave engineering, nanotechnology, electrochemistry, renewable energies, mechatronics/control, and electrical materials science.
+      Electrical engineers typically hold a degree in electrical engineering or electronic engineering. Practising engineers may have professional certification and be members of a professional body or an international standards organization. These include the International Electrotechnical Commission (IEC), the Institute of Electrical and Electronics Engineers (IEEE) and the Institution of Engineering and Technology `,
     name: "Electrical Technology",
     subpath: "Electrical",
     firstShift: {
-      src: "/sir/pavel.jpg",
+      src: "/Electrical/sir/pabel-sir.jpg",
       name: "Reaz Haider Pavel",
-      address: "Comilla Sadar, Comilla",
+      address: "Comilla ",
       facebook: "https://www.facebook.com/ReazHaiderPavel",
       gmail: "#",
     },
     secondShift: {
-      src: "/sir/kakon.JPG",
-      name: "Unkown Head",
-      address: "Unkown, Unkown",
+      src: "/sir/nasir-sir.JPG",
+      name: "Kakon Chandra Das",
+      address: "Maijdee,Noakhali",
       facebook: "https://www.facebook.com/kakan.das2",
       gmail: "#",
     },
@@ -47,22 +46,21 @@ export default {
     },
 
     {
-      name: "Md Sazzad Hossain Rifat",
+      name: "Md.Sazzad Hossain Rifat",
       title: "Craft Instructor",
       src: "/sir/Sazzad.JPG",
       facebook: "#",
       gmail: "#",
     },
     {
-      name: "Md Mohsin Hossain",
+      name: "Md.Mohsin Hossain",
       title: "Craft Instructor",
       src: "/sir/mohsin.JPG",
       facebook: "#",
       gmail: "#",
     },
-
     {
-      name: "Md Nahidul Houqe",
+      name: "Md.Nahidul Houqe",
       title: "Craft Instructor",
       src: "/sir/nahidul.JPG",
       facebook: "#",

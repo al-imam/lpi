f58f2c70b3ce0ffc8a7b3153d952cc770d4e5cdc
al-imam <alimam01828@gmail.com>
2023-01-20 02:23:42 +0600

    change data aidt

--- src/pages/Departments/Architectural/data.js
+++ src/pages/Departments/Architectural/data.js
@@ -1,12 +1,12 @@
 export default {
   heading: {
-    src: "/assets/collage.jpg",
+    src: "/AIDT/civil-4.JPG",
     description: `Architectural engineering, also known as building engineering or architecture engineering, is an engineering discipline that deals with the technological aspects and multi-disciplinary.
-    From reduction of greenhouse gas emissions to the construction of resilient buildings, architectural engineers are at the forefront of addressing several major challenges of the 21st century. They apply the latest scientific knowledge and technologies to the design of buildings. Architectural engineering as a relatively new licensed profession emerged in the 20th century as a result of the rapid technological developments. Architectural engineers are at the forefront of two major historical opportunities that today's world is immersed in: (1) that of rapidly advancing computer-technology, and (2) the parallel revolution arising from the need to create a sustainable planet.
-    Distinguished from architecture as an art of design, architectural engineering is the art and science of engineering and construction as practiced in respect of buildings.
-    Structural engineering involves the analysis and design of the built environment (buildings, bridges, equipment supports, towers and walls). Those concentrating on buildings are sometimes informally referred to as "building engineers". Structural engineers require expertise in strength of materials, structural analysis, and in predicting structural load such as from weight of the building, occupants and contents, and extreme events such as wind, rain, ice, and seismic design of structures which is referred to as earthquake engineering. Architectural Engineers sometimes incorporate structural as one aspect of their designs; the structural discipline when practiced as a specialty works closely with architects and other engineering specialists. `,
+      From reduction of greenhouse gas emissions to the construction of resilient buildings, architectural engineers are at the forefront of addressing several major challenges of the 21st century. They apply the latest scientific knowledge and technologies to the design of buildings. Architectural engineering as a relatively new licensed profession emerged in the 20th century as a result of the rapid technological developments. Architectural engineers are at the forefront of two major historical opportunities that today's world is immersed in: (1) that of rapidly advancing computer-technology, and (2) the parallel revolution arising from the need to create a sustainable planet.
+      Distinguished from architecture as an art of design, architectural engineering is the art and science of engineering and construction as practiced in respect of buildings.
+      Structural engineering involves the analysis and design of the built environment (buildings, bridges, equipment supports, towers and walls). Those concentrating on buildings are sometimes informally referred to as "building engineers". Structural engineers require expertise in strength of materials, structural analysis, and in predicting structural load such as from weight of the building, occupants and contents, and extreme events such as wind, rain, ice, and seismic design of structures which is referred to as earthquake engineering. Architectural Engineers sometimes incorporate structural as one aspect of their designs; the structural discipline when practiced as a specialty works closely with architects and other engineering specialists. `,
     name: "Architectur technology",
-    subpath: "Architectur",
+    subpath: "architecture",
     firstShift: {
       src: "/sir/pavel.jpg",
       name: "Md.Alomgir Hossain",
@@ -25,14 +25,14 @@ export default {
 
   teacher: [
     {
-      name: "Md Abu Obaida",
+      name: "Md.Abu Obaida",
       title: "Junior Instructor",
-      src: "/sir/obaida.JPG",
+      src: "/AIDT/sir/obayda-sir.jpg",
       facebook: "#",
       gmail: "#",
     },
     {
-      name: "Md Murad Hosen",
+      name: "Md.Murad Hosen",
       title: "Junior Instructor",
       src: "/sir/murad.JPG",
       facebook: "#",
@@ -49,7 +49,7 @@ export default {
     {
       name: "Mijanur Rahman",
       title: "Craft Instructor",
-      src: "/sir/mijanur.JPG",
+      src: "/AIDT/sir/mijanur-sir.JPG",
       facebook: "#",
       gmail: "#",
     },
@@ -62,16 +62,23 @@ export default {
       gmail: "#",
     },
     {
-      name: "Md Imran Hossain",
+      name: "Md.Imran Hossain",
       title: "Craft Instructor",
-      src: "/sir/imran.JPG",
+      src: "/AIDT/sir/imran-sir.JPG",
       facebook: "#",
       gmail: "#",
     },
     {
-      name: "Md Mohsin",
+      name: "Md.Mohsin",
       title: "Craft Instructor",
       src: "/sir/mohsin.JPG",
+      facebook: "#",
+      gmail: "#",
+    },
+    {
+      name: "Roksana Yesmin",
+      title: "Lab Assistant",
+      src: "/AIDT/sir/Roksana-mam.JPG",
       facebook: "#",
       gmail: "#",
     },

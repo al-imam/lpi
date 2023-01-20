0fbf6a6df67e048d066ab3e17fd17d3782f75331
al-imam <alimam01828@gmail.com>
2023-01-20 22:03:49 +0600

    make layout responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -31,7 +31,7 @@ function Main() {
         </div>
         <div className={classes.card}>
           <div className={classes.cardContainer}>
-            <img src="/Non-tech/sir/principal-sir.jpg" />
+            <img src="/public/Non-tech/sir/atik-sir.JPG" />
             <div className={classes.info}>
               <div className={classes.heading}>
                 <h4>Mohammad Alauddin</h4>

93d3e31ce476623ba39414b8bd57601b0899d7ac
al-imam <alimam01828@gmail.com>
2023-01-20 21:34:04 +0600

    create main teacher card

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,58 @@
 import classes from "./main.module.css";
+import MailLockIcon from "@mui/icons-material/MailLock";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 function Main() {
   return (
-    <div className={classes.content}>
-      <div className={classes.card}>
-        <div className={classes.cardContainer}>
-          <img src="/Non-tech/sir/principal-sir.jpg" />
-          <div className={classes.info}>
-            <h4>i don't know</h4>
-            <span>principal</span>
-            <p className={classes.location}>fjghduyguydgh</p>
+    <section className={classes.main}>
+      <div className={classes.content}>
+        <div className={classes.card}>
+          <div className={classes.cardContainer}>
+            <img src="/Non-tech/sir/principal-sir.jpg" />
+            <div className={classes.info}>
+              <div className={classes.heading}>
+                <h4>Mohammad Alauddin</h4>
+                <span className={classes.title}>Principal</span>
+              </div>
+              <div className={classes.social}>
+                <address className={classes.location}>
+                  <LocationOnIcon /> comilla, comilla sadar
+                </address>
+                <a href="#" className={classes.address}>
+                  <FacebookIcon /> Facebook
+                </a>
+                <a href="#" className={classes.address}>
+                  <MailLockIcon /> Mail
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.cardContainer}>
+            <img src="/Non-tech/sir/principal-sir.jpg" />
+            <div className={classes.info}>
+              <div className={classes.heading}>
+                <h4>Mohammad Alauddin</h4>
+                <span className={classes.title}>Principal</span>
+              </div>
+              <div className={classes.social}>
+                <address className={classes.location}>
+                  <LocationOnIcon /> comilla, comilla sadar
+                </address>
+                <a href="#" className={classes.address}>
+                  <FacebookIcon /> Facebook
+                </a>
+                <a href="#" className={classes.address}>
+                  <MailLockIcon /> Mail
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="badgescard"></div>
-    </div>
+    </section>
   );
 }
 

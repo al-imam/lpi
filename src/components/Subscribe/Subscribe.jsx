64758822c9243cdfaf218334170e5655299d1ceb
al-imam <alimam01828@gmail.com>
2023-01-21 00:42:06 +0600

    style subscribe section

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
+import classes from "./sub.module.css";
 
 function Subscribe() {
   const [value, setValue] = useState("");
   return (
     <section style={{ paddingInline: "var(--gap-inline)" }}>
-      <div>
-        <p>Subscribe for future Notice's and updates</p>
-        <form>
-          <input
-            type="email"
-            name="subscribe"
-            value={value}
-            onChange={(evt) => setValue(evt.target.value)}
-          />
-          <button>Subscribe</button>
-        </form>
+      <div className={classes.container}>
+        <div className={classes.main}>
+          <div className={classes.text}>
+            <h5>Subscribe</h5>
+            <span>Subscribe for future Notice's and updates!</span>
+          </div>
+          <form className={classes.form}>
+            <input
+              className={classes.input}
+              type="email"
+              name="subscribe"
+              value={value}
+              onChange={(evt) => setValue(evt.target.value)}
+            />
+            <button className={classes.button + " " + classes.input}>
+              Subscribe
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );

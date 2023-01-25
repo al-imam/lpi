5ede10e955f7eaf3860fdd6b39cf844e4ab150e2
al-imam <alimam01828@gmail.com>
2023-01-25 18:57:26 +0600

    style container add gaping and many more

--- src/components/RelatedLinks/RelatedLinks.jsx
+++ src/components/RelatedLinks/RelatedLinks.jsx
@@ -3,39 +3,45 @@ import classes from "./rl.module.css";
 function RelatedLinks() {
   return (
     <section className={classes.section}>
-      <h2 className={classes.heading}>Related Links</h2>
-      <ul className={classes.items}>
-        <li>
-          <a target="_blank" rel="noopener" href="http://www.techedu.gov.bd">
-            Directorate Of Technical Education
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noopener" href="http://www.bteb.gov.bd">
-            Bangladesh Technical Education Board (BTEB)
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noopener" href="http://www.moedu.gov.bd">
-            Ministry of Education
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noopener" href="http://www.step-dte.gov.bd/">
-            Skills and Training Enhancement Project
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noopener" href="http://www.bpsc.gov.bd/">
-            Bangladesh Public Service Commission
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noopener" href="http://www.bcc.gov.bd/">
-            Bangladesh Computer Council
-          </a>
-        </li>
-      </ul>
+      <div className={classes.container}>
+        <h2 className={classes.heading}>Related Links</h2>
+        <ul className={classes.items}>
+          <li>
+            <a target="_blank" rel="noopener" href="http://www.techedu.gov.bd">
+              Directorate Of Technical Education
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noopener" href="http://www.bteb.gov.bd">
+              Bangladesh Technical Education Board (BTEB)
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noopener" href="http://www.moedu.gov.bd">
+              Ministry of Education
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener"
+              href="http://www.step-dte.gov.bd/"
+            >
+              Skills and Training Enhancement Project
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noopener" href="http://www.bpsc.gov.bd/">
+              Bangladesh Public Service Commission
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noopener" href="http://www.bcc.gov.bd/">
+              Bangladesh Computer Council
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }

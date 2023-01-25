cd87a7980346388dfa196be37bfe53dd500aa334
al-imam <alimam01828@gmail.com>
2023-01-26 01:11:25 +0600

    style section

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -1,6 +1,8 @@
+import classes from "./marquee.module.css";
+
 function Marquee() {
   return (
-    <section>
+    <section className={classes.section}>
       <p>
         আগামী ২৮/০১/২০২৩ ইং হইতে অনুষ্ঠিতব্য পর্ব সমাপনি পরীক্ষায় অংশগ্রহনকারী
         সকল শিক্ষার্থীদের পরীক্ষায় অংশগ্রহনের উদ্দেশ্যে নির্ধারিত সময়ের কমপক্ষে

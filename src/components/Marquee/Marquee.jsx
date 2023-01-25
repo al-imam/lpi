68377481a50f2928e19e3b324b6970670413704c
al-imam <alimam01828@gmail.com>
2023-01-26 00:47:58 +0600

    create dummy component

---
+++ src/components/Marquee/Marquee.jsx
@@ -0,0 +1,14 @@
+function Marquee() {
+  return (
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, iure. Amet
+        consequatur suscipit accusamus officia sit qui voluptatibus eveniet
+        aliquid magni corporis dolore minus, deserunt nemo, aspernatur rerum
+        earum enim?
+      </p>
+    </section>
+  );
+}
+
+export default Marquee;

7d1efc0932e3ead5117f285d38b11f38a713bb86
al-imam <alimam01828@gmail.com>
2023-01-25 18:48:29 +0600

    add semantic html

--- src/components/RelatedLinks/RelatedLinks.jsx
+++ src/components/RelatedLinks/RelatedLinks.jsx
@@ -1,46 +1,40 @@
 function RelatedLinks() {
   return (
-    <div>
+    <section>
       <h2>Related Links</h2>
-      <div>
-        <ul>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.techedu.gov.bd">
-              Directorate Of Technical Education
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.bteb.gov.bd">
-              Bangladesh Technical Education Board (BTEB)
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.moedu.gov.bd">
-              Ministry of Education
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener"
-              href="http://www.step-dte.gov.bd/"
-            >
-              Skills and Training Enhancement Project
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.bpsc.gov.bd/">
-              Bangladesh Public Service Commission
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.bcc.gov.bd/">
-              Bangladesh Computer Council
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+      <ul>
+        <li>
+          <a target="_blank" rel="noopener" href="http://www.techedu.gov.bd">
+            Directorate Of Technical Education
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noopener" href="http://www.bteb.gov.bd">
+            Bangladesh Technical Education Board (BTEB)
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noopener" href="http://www.moedu.gov.bd">
+            Ministry of Education
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noopener" href="http://www.step-dte.gov.bd/">
+            Skills and Training Enhancement Project
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noopener" href="http://www.bpsc.gov.bd/">
+            Bangladesh Public Service Commission
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noopener" href="http://www.bcc.gov.bd/">
+            Bangladesh Computer Council
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 }
 

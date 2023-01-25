44eff51230d3ac977ff1ca735be8c53b417922b4
al-imam <alimam01828@gmail.com>
2023-01-26 01:13:34 +0600

    style actual text

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -3,7 +3,7 @@ import classes from "./marquee.module.css";
 function Marquee() {
   return (
     <section className={classes.section}>
-      <p>
+      <p className={classes.text}>
         আগামী ২৮/০১/২০২৩ ইং হইতে অনুষ্ঠিতব্য পর্ব সমাপনি পরীক্ষায় অংশগ্রহনকারী
         সকল শিক্ষার্থীদের পরীক্ষায় অংশগ্রহনের উদ্দেশ্যে নির্ধারিত সময়ের কমপক্ষে
         ৩০ মিনিট পূর্বে হলে প্রবেশ করার জন্য বলা হচ্ছে।

c590605b144555c763524062f4f72a1868cf1f5b
al-imam <alimam01828@gmail.com>
2023-01-27 13:00:23 +0600

    make relatedLink component reuseable

--- src/components/RelatedLinks/RelatedLinks.jsx
+++ src/components/RelatedLinks/RelatedLinks.jsx
@@ -1,45 +1,18 @@
 import classes from "./rl.module.css";
 
-function RelatedLinks() {
+function RelatedLinks({ data }) {
   return (
     <section className={classes.section}>
       <div className={classes.container}>
-        <h2 className={classes.heading}>Related Links</h2>
+        <h2 className={classes.heading}>{data.heading}</h2>
         <ul className={classes.items}>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.techedu.gov.bd">
-              Directorate Of Technical Education
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.bteb.gov.bd">
-              Bangladesh Technical Education Board
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.moedu.gov.bd">
-              Ministry of Education
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener"
-              href="http://www.step-dte.gov.bd/"
-            >
-              Skills and Training Enhancement Project
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.bpsc.gov.bd/">
-              Bangladesh Public Service Commission
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener" href="http://www.bcc.gov.bd/">
-              Bangladesh Computer Council
-            </a>
-          </li>
+          {data.links.map((item) => (
+            <li>
+              <a target="_blank" rel="noopener" href={item.link}>
+                {item.text}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </section>

71792eb922a3c92438e19637e3133c14956a2b2c
al-imam <alimam01828@gmail.com>
2023-01-27 12:58:34 +0600

    move data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default {
+  heading: "Related Links",
+  links: [
+    {
+      text: "Directorate Of Technical Education",
+      link: "http://www.techedu.gov.bd",
+    },
+    {
+      text: "Bangladesh Technical Education Board",
+      link: "http://www.bteb.gov.bd",
+    },
+    {
+      text: "Ministry of Education",
+      link: "http://www.moedu.gov.bd",
+    },
+    {
+      text: "Skills and Training Enhancement Project",
+      link: "http://www.step-dte.gov.bd/",
+    },
+    {
+      text: "Bangladesh Public Service Commission",
+      link: "http://www.bpsc.gov.bd/",
+    },
+    {
+      text: "Bangladesh Computer Council",
+      link: "http://www.bcc.gov.bd/",
+    },
+  ],
+};

9c7482d1943d56337e8f20ac63c690120f492e18
al-imam <alimam01828@gmail.com>
2023-01-27 13:15:19 +0600

    reuse Link component in recentNotice component

--- src/components/RecentNotice/RecentNotice.jsx
+++ src/components/RecentNotice/RecentNotice.jsx
@@ -1,52 +1,8 @@
-import classes from "./recentNotice.module.css";
+import Links from "../Links/Links";
+import data from "./data";
 
 function RecentNotice() {
-  return (
-    <div className={classes.RecentNotice}>
-      <h4 className={classes.heading}>Recent Notice</h4>
-      <ul>
-        <li>
-          <a>
-            ডিপ্লোমা পর্যায়ের শিক্ষাক্রমে সরকারি প্রতিষ্ঠানসমূহে ভর্তি সংক্রান্ত
-            বিজ্ঞপ্তি
-          </a>
-        </li>
-        <li>
-          <a>অভিনন্দনবার্তা</a>
-        </li>
-        <li>
-          <a>New Year Greetings</a>
-        </li>
-        <li>
-          <a>শোকবার্তা</a>
-        </li>
-        <li>
-          <a>শীতকালিন ক্লাসের সময় পরিবর্তন সংক্রান্ত নোটিশ</a>
-        </li>
-        <li>
-          <a>পরীক্ষার ফরম ফিলাপের বিজ্ঞপ্তি</a>
-        </li>
-        <li>
-          <a>নতুন সেশনে ভর্তি সংক্রান্ত নোটিশ</a>
-        </li>
-        <li>
-          <a>মধ্যপর্ব পরীক্ষার সংশোধিত রুটিন</a>
-        </li>
-        <li>
-          <a>মধ্যপর্ব পরীক্ষার রুটিন সংশোধনী নোটিশ</a>
-        </li>
-        <li>
-          <a>
-            Presidency University আয়োজিত Inter-College English Language Contest
-            2022 এ অংশগ্রহণের আহবান
-          </a>
-        </li>
-      </ul>
-      <div>
-        <a>View all Notice</a>
-      </div>
-    </div>
-  );
+  return <Links data={data} />;
 }
 
 export default RecentNotice;

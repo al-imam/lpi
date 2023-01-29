0a5f3043063bfd383461042c1f191265093a2540
al-imam <alimam01828@gmail.com>
2023-01-29 20:48:15 +0600

    create NoticeAndNews component to show both

---
+++ src/components/NoticeAndNews/NoticeAndNews.jsx
@@ -0,0 +1,13 @@
+import RecentNotice from "../RecentNotice/RecentNotice";
+import News from "../News/News";
+
+function NoticeAndNews() {
+  return (
+    <section>
+      <RecentNotice />
+      <News />
+    </section>
+  );
+}
+
+export default NoticeAndNews;

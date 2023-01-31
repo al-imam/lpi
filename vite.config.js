0bf3ab333af8ea258dc76edc051e929ac50ce521
al-imam <alimam01828@gmail.com>
2023-01-31 22:46:24 +0600

    vite map paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@util": path.resolve(__dirname, "src/util"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@department": path.resolve(__dirname, "src/pages/Departments"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@assets": path.resolve(__dirname, "/public"),
+    },
+  },
+});

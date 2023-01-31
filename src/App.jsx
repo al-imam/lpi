2288242477a844cfa1be9c92f27327a78a54cc2c
al-imam <alimam01828@gmail.com>
2023-01-31 22:47:45 +0600

    use Map path avoid relative path

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import Head from "./components/Head/Head";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-import Departments from "./pages/Departments/Departments";
-import Loader from "./components/Loader/Loader";
-import Computer from "./pages/Departments/Computer/Computer";
-import Civil from "./pages/Departments/Civil/Civil";
-import Electrical from "./pages/Departments/Electrical/Electrical";
-import Electronics from "./pages/Departments/Electronics/Electronics";
-import Architectural from "./pages/Departments/Architectural/Architectural";
-import Contact from "./pages/Contact/Contact";
-import ScrollToTop from "./util/ScrollToTop";
-import Syllabus from "./pages/Syllabus/Syllabus";
-import Open from "./pages/Facility/Open/Open";
-import Lab from "./pages/Facility/Lab/Lab";
-const About = lazy(() => import("./pages/About/About"));
+import Head from "@components/Head/Head";
+import Footer from "@components/Footer/Footer";
+import Home from "@pages/Home/Home";
+import Departments from "@department/Departments";
+import Loader from "@components/Loader/Loader";
+import Computer from "@department/Computer/Computer";
+import Civil from "@department/Civil/Civil";
+import Electrical from "@department/Electrical/Electrical";
+import Electronics from "@department/Electronics/Electronics";
+import Architectural from "@department/Architectural/Architectural";
+import Contact from "@pages/Contact/Contact";
+import ScrollToTop from "@util/ScrollToTop";
+import Syllabus from "@pages/Syllabus/Syllabus";
+import Open from "@pages/Facility/Open/Open";
+import Lab from "@pages/Facility/Lab/Lab";
+const About = lazy(() => import("@pages/About/About"));
 
-import Links from "./components/Links/Links";
-import data from "./data";
-import Section from "./components/Section/Section";
+import Links from "@components/Links/Links";
+import data from "@app/data";
+import Section from "@components/Section/Section";
 
 function App() {
   return (

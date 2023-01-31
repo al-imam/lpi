458c688a7bff42dc6e09107d5700755f5228c72a
al-imam <alimam01828@gmail.com>
2023-01-31 22:51:08 +0600

    update path use map path

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense } from "react";
-import Location from "../../components/Location/Location";
-import AboutUs from "../../components/AboutUs/AboutUs";
-import Status from "../../components/Status/Status";
-import Loader from "../../components/Loader/Loader";
+import Location from "@components/Location/Location";
+import AboutUs from "@components/AboutUs/AboutUs";
+import Status from "@components/Status/Status";
+import Loader from "@components/Loader/Loader";
 import data from "./data";
 
-const Gallery = lazy(() => import("../../components/Gallery/Gallery"));
+const Gallery = lazy(() => import("@components/Gallery/Gallery"));
 
 function About() {
   return (

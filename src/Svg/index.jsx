91f7108c8566d4c2fd47a17e4bbc6a93fa18e4e0
al-imam <alimam01828@gmail.com>
2023-02-01 17:41:03 +0600

    Fix typo

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -4,7 +4,7 @@ export function Email(props) {
       xmlns="http://www.w3.org/2000/svg"
       width="32"
       height="32"
-      viewBox="0 0 24 24"
+      viewBox="0 0 21 21"
       {...props}
     >
       <g

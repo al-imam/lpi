e6681654e598e944f6a470f1281180463e017bdf
al-imam <alimam01828@gmail.com>
2023-02-01 17:45:53 +0600

    add write icons

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -40,3 +40,23 @@ export function Mobile(props) {
     </svg>
   );
 }
+
+export function Write(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17 4a2.121 2.121 0 0 1 0 3l-9.5 9.5l-4 1l1-3.944l9.504-9.552a2.116 2.116 0 0 1 2.864-.125zM9.5 17.5h8m-2-11l1 1"
+      ></path>
+    </svg>
+  );
+}

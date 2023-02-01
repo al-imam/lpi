066938652b1bd3b29de93c526801fdcaad04f696
al-imam <alimam01828@gmail.com>
2023-02-01 17:22:28 +0600

    create email svg

---
+++ src/Svg/index.jsx
@@ -0,0 +1,22 @@
+export function Email(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M12.5 5.5h-8a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-5"></path>
+        <path d="m4.5 8.5l5 3l5-3m2-5v4m-2-2h4"></path>
+      </g>
+    </svg>
+  );
+}

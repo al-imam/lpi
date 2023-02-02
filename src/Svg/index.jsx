1fd85580d28a77f9fb42c7ae49c2e3a6a3dad8d6
al-imam <alimam01828@gmail.com>
2023-02-02 19:03:28 +0600

    add location svg

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -114,3 +114,27 @@ export function ArrowRight(props) {
     </svg>
   );
 }
+
+export function Location(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        transform="translate(4 2)"
+      >
+        <path d="m6.5 16.54l.631-.711c.716-.82 1.36-1.598 1.933-2.338l.473-.624c1.975-2.661 2.963-4.773 2.963-6.334C12.5 3.201 9.814.5 6.5.5s-6 2.701-6 6.033c0 1.561.988 3.673 2.963 6.334l.473.624a54.84 54.84 0 0 0 2.564 3.05z"></path>
+        <circle cx="6.5" cy="6.5" r="2.5"></circle>
+      </g>
+    </svg>
+  );
+}

6c55c13126b80a704edb72b5edafc217dd67aeca
al-imam <alimam01828@gmail.com>
2023-02-02 21:42:03 +0600

    remove mui icon from contact page

--- src/components/Footer/ContactInfo/ContactInfo.jsx
+++ src/components/Footer/ContactInfo/ContactInfo.jsx
@@ -1,14 +1,11 @@
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import MailLockIcon from "@mui/icons-material/MailLock";
-import PhonelinkLockIcon from "@mui/icons-material/PhonelinkLock";
-
+import { Mobile, Email, Location } from "@svg";
 import classes from "./contactInfo.module.css";
 
 function ContactInfo() {
   return (
     <div className={classes.footerContact}>
       <div className={classes.contact}>
-        <LocationOnIcon />
+        <Location />
         <div className={classes.contactInfo}>
           <h4 className={classes.infoHeading}>Find us</h4>
           <address className={classes.infoDescription}>
@@ -17,7 +14,7 @@ function ContactInfo() {
         </div>
       </div>
       <div className={classes.contact}>
-        <PhonelinkLockIcon />
+        <Mobile />
         <div className={classes.contactInfo}>
           <h4 className={classes.infoHeading}>Call us</h4>
           <a className={classes.infoDescription} href="tel:+8801881614926">
@@ -26,7 +23,7 @@ function ContactInfo() {
         </div>
       </div>
       <div className={classes.contact}>
-        <MailLockIcon />
+        <Email />
         <div className={classes.contactInfo}>
           <h4 className={classes.infoHeading}>Mail us</h4>
           <a

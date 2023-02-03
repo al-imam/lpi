c1a10759ba5c1bd7146abcdbe7bf86863e4af9c5
al-imam <alimam01828@gmail.com>
2023-02-03 17:28:08 +0600

    refactor about page css and jsx

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,21 +1,14 @@
 import classes from "./aboutUs.module.css";
 import { NavLink } from "react-router-dom";
 
-function AboutUs({ subtitle, link, direction = "row" }) {
+function AboutUs({ subtitle }) {
   return (
-    <section className={classes.q}>
-      <div className={classes.section}>
-        <div className={classes.container + " " + classes[direction]}>
-          <img className={classes.image} src="/assets/main.jpg" alt="collage" />
-          <div className={classes.info}>
-            <h3 className={classes.title}>About LPI</h3>
-            <p className={classes.subtitle}>{subtitle}</p>
-            {link ? (
-              <NavLink className={classes.link} to="/about">
-                Learn more
-              </NavLink>
-            ) : null}
-          </div>
+    <section className={classes.section}>
+      <div className={classes.container}>
+        <img className={classes.image} src="/assets/main.jpg" alt="collage" />
+        <div className={classes.info}>
+          <h3 className={classes.title}>About LPI</h3>
+          <p className={classes.subtitle}>{subtitle}</p>
         </div>
       </div>
     </section>

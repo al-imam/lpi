cba99199b80a59e8a613a6cbef66fbdc7b284140
al-imam <alimam01828@gmail.com>
2023-02-06 00:30:25 +0600

    Fix Dropdown Not close on option click bug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { ArrowDown } from "@svg";
 import { useNavigate, NavLink } from "react-router-dom";
 
 import data from "./data";
+import { useState } from "react";
 
 function Navbar({ open }) {
   return (
@@ -30,35 +31,56 @@ function Navbar({ open }) {
 
 export default Navbar;
 
+const init = {
+  hover: false,
+  focus: false,
+};
+
 function DropDownMenu({ root }) {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(init);
 
   return (
-    <li className={classes.navItem}>
+    <li
+      className={classes.navItem}
+      onMouseEnter={() => setIsOpen({ ...isOpen, hover: true })}
+      onMouseLeave={() => setIsOpen({ ...isOpen, hover: false })}
+      onFocus={() => setIsOpen({ hover: false, focus: true })}
+      onBlur={() => setIsOpen({ hover: false, focus: false })}
+    >
       <button
         className={classes.dropdownToggler}
         onClick={() => navigate(root.path)}
       >
         {root.text} <ArrowDown />
       </button>
-      <DropDown routes={root.routes} />
+      <DropDown routes={root.routes} isOpen={isOpen} setIsOpen={setIsOpen} />
     </li>
   );
 }
 
-function DropDown({ routes }) {
+function DropDown({ routes, isOpen, setIsOpen }) {
   return (
-    <ul className={classes.dropdownMenu}>
+    <ul
+      className={`${classes.dropdownMenu} ${
+        isOpen.hover || isOpen.focus ? classes.showMenu : ""
+      }`}
+    >
       {routes.map((el) => (
-        <DeepLi path={el.path} text={el.text} key={el.text} />
+        <DeepLi
+          path={el.path}
+          text={el.text}
+          key={el.text}
+          setIsOpen={setIsOpen}
+        />
       ))}
     </ul>
   );
 }
 
-function DeepLi({ text, path }) {
+function DeepLi({ text, path, setIsOpen }) {
   return (
-    <li>
+    <li onClick={() => setIsOpen({ hover: false, focus: false })}>
       <NavLink to={path}>{text}</NavLink>
     </li>
   );

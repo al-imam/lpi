0b2c5f72375d2cbbb3bcb72fca719bf7441c6ec2
al-imam <alimam01828@gmail.com>
2023-02-08 09:12:23 +0600

    create post markup

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,7 +1,17 @@
 function Post() {
   return (
     <div>
-      <h1>components</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          voluptates praesentium enim. Velit veritatis quasi dicta, omnis eaque
+          qui numquam.
+        </p>
+        <div>
+          <img src="/assets/main.jpg" alt="Post image" />
+        </div>
+      </div>
     </div>
   );
 }

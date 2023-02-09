518c509cff9e8167c660728b818ffb41a8bf0a3d
al-imam <alimam01828@gmail.com>
2023-02-09 12:25:27 +0600

    change layout again

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,27 +3,26 @@ import classes from "./post.module.css";
 function Post() {
   return (
     <div className={classes.post}>
-      <div className={classes.postContainer}>
-        <div className={classes.image}>
-          <p className={classes.title}>
-            Lorem ipsum dolor sit amet. <span>{new Date().toDateString()}</span>
-          </p>
-          <img
-            src="https://cbseguides.com/wp-content/uploads/2022/03/Notice-Writing.jpg"
-            alt="Post image"
-          />
-          <a href="/public/assets/lab.jpg" download={true}>
-            ⬇️
-          </a>
-        </div>
-        <p className={classes.subtitle}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-          voluptates praesentium enim. Velit veritatis quasi dicta, omnis eaque
-          qui numquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Odit obcaecati accusantium recusandae aut asperiores magni dignissimos
-          error consequatur expedita iusto?
-        </p>
+      <p className={classes.title}>
+        <span className={classes.text}>Lorem ipsum dolor sit amet.</span>
+        <time className={classes.time}>{new Date().toDateString()}</time>
+      </p>
+      <div className={classes.image}>
+        <img
+          src="https://cbseguides.com/wp-content/uploads/2022/03/Notice-Writing.jpg"
+          alt="Post image"
+        />
+        <a href="/public/assets/lab.jpg" download={true}>
+          ⬇️
+        </a>
       </div>
+      <p className={classes.subtitle}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+        voluptates praesentium enim. Velit veritatis quasi dicta, omnis eaque
+        qui numquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Odit obcaecati accusantium recusandae aut asperiores magni dignissimos
+        error consequatur expedita iusto?
+      </p>
     </div>
   );
 }

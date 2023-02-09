6cb2eedaa7cad8b653da22fe71e4b3def302fc29
al-imam <alimam01828@gmail.com>
2023-02-09 11:41:32 +0600

    create news page component

---
+++ src/pages/News/News.jsx
@@ -0,0 +1,15 @@
+import Post from "../../components/Post/Post";
+
+function News() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
+
+export default News;

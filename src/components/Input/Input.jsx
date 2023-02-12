10bcb9aea788148da7fa65403f5bbda92f677456
al-imam <alimam01828@gmail.com>
2023-02-12 10:32:16 +0600

    Fix typo

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -11,7 +11,7 @@ function Input({
 }) {
   return (
     <input
-      spellcheck="false"
+      spellCheck="false"
       value={value}
       onChange={(evt) => setValue(evt.target.value)}
       className={`${classes.inputs} ${classes.text} ${classes[width]}`}
@@ -31,7 +31,7 @@ export function Textarea({
 }) {
   return (
     <textarea
-      spellcheck="false"
+      spellCheck="false"
       value={value}
       onChange={(evt) => setValue(evt.target.value)}
       rows={rows}
@@ -55,7 +55,7 @@ export function PasswordInput({ placeholder, setValue, value }) {
       <input
         onFocus={() => setIsFocus(true)}
         onBlur={() => setIsFocus(false)}
-        spellcheck="false"
+        spellCheck="false"
         className={classes.inputs + " " + classes.password}
         value={value}
         onChange={(evt) => setValue(evt.target.value)}

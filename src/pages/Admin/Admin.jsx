b9823e5bd65b41f8b8e742e620fdae6cb39fcf1c
al-imam <alimam01828@gmail.com>
2023-02-13 13:30:45 +0600

    create admin page template

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,9 +1,65 @@
-function Admin() {
+import Input, { Textarea } from "@components/Input/Input";
+import { useReducer } from "react";
+import classes from "./admin.module.css";
+
+export default function Contact() {
   return (
-    <main>
-      <section></section>
+    <main className={classes.s}>
+      <section className={classes.wrapper}>
+        <div className={classes.title}>
+          <h4>Login as admin</h4>
+          <p>
+            For security reasons, it's important to keep your login credentials
+            safe and secure. If you're new to the dashboard, please contact the
+            admin to obtain an account.
+          </p>
+        </div>
+        <Form />
+      </section>
     </main>
   );
 }
 
-export default Admin;
+const init = {
+  title: "",
+  description: "",
+};
+
+function reducer(prevState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "title":
+      return {
+        ...prevState,
+        title: payload,
+      };
+    case "description":
+      return {
+        ...prevState,
+        description: payload,
+      };
+    default:
+      throw new Error(`No Action called ${type}`);
+  }
+}
+
+function Form() {
+  const [{ title, description }, dispatch] = useReducer(reducer, init);
+
+  return (
+    <form className={classes.form} onSubmit={(e) => e.preventDefault()}>
+      <Input
+        value={title}
+        setValue={(value) => dispatch({ type: "title", payload: value })}
+        type="text"
+        placeholder="Title"
+      />
+      <Textarea
+        value={description}
+        setValue={(value) => dispatch({ type: "description", payload: value })}
+        placeholder="Say more about current News or Notice"
+      />
+      <Input type="submit" value="Post" />
+    </form>
+  );
+}

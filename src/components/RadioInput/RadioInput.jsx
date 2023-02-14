2229d50b549915811e1a409c601dd052a667df46
al-imam <alimam01828@gmail.com>
2023-02-14 15:37:51 +0600

    take more props

--- src/components/RadioInput/RadioInput.jsx
+++ src/components/RadioInput/RadioInput.jsx
@@ -1,13 +1,21 @@
 import { useId } from "react";
 import classes from "./radio.module.css";
 
-function RadioInput({ text, name }) {
+function RadioInput({ value, name, ...rest }) {
   const id = useId();
+
   return (
     <div className={classes.container}>
-      <input type="radio" id={id} name={name} className={classes.radio} />
+      <input
+        value={value}
+        name={name}
+        type="radio"
+        id={id}
+        className={classes.radio}
+        {...rest}
+      />
       <label htmlFor={id} className={classes.label}>
-        {text}
+        {value}
       </label>
     </div>
   );

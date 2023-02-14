835445f3a83b5dbb44c86699467b4c228c82bb06
al-imam <alimam01828@gmail.com>
2023-02-14 16:35:33 +0600

    add name attribute

--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -3,7 +3,7 @@ import classes from "./fileInput.module.css";
 function FileInput() {
   return (
     <div class={classes.container}>
-      <input type="file" className={classes.input} />
+      <input type="file" name="file" className={classes.input} />
     </div>
   );
 }

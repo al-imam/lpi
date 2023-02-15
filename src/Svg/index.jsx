5f3523800437709d107587a23bc95fbf4e3a7613
al-imam <alimam01828@gmail.com>
2023-02-15 09:42:59 +0600

    add new trash icon

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -310,3 +310,23 @@ export function EyeOpen(props) {
     </svg>
   );
 }
+
+export function Trash(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5.5 4.5h10v12a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2zm5-2a2 2 0 0 1 1.995 1.85l.005.15h-4a2 2 0 0 1 2-2zm-7 2h14m-9 3v8m4-8v8"
+      ></path>
+    </svg>
+  );
+}

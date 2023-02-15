2347382dc23e41285690234a048868bbc6b1e57a
al-imam <alimam01828@gmail.com>
2023-02-15 21:58:55 +0600

    export only apps from firebase init file

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -13,6 +10,3 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);

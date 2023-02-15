bfa29a0d08ce99819a375ed9af1bc1a4e439199d
al-imam <alimam01828@gmail.com>
2023-02-15 22:13:30 +0600

    remove form from admin component

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,15 +1,5 @@
-import Input, { Textarea } from "@components/Input/Input";
-import RadioInput from "@components/RadioInput/RadioInput";
-import { useReducer } from "react";
-import FileInput from "@components/FileInput/FileInput";
 import classes from "./admin.module.css";
-import { app } from "../../firebase";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  serverTimestamp,
-} from "firebase/firestore";
+import Form from "./Form";
 
 export default function Contact() {
   return (
@@ -27,75 +17,5 @@ export default function Contact() {
         <Form />
       </section>
     </main>
-  );
-}
-
-const init = {
-  title: "",
-  description: "",
-};
-
-function reducer(prevState, action) {
-  const { type, payload } = action;
-  switch (type) {
-    case "title":
-      return {
-        ...prevState,
-        title: payload,
-      };
-    case "description":
-      return {
-        ...prevState,
-        description: payload,
-      };
-    default:
-      throw new Error(`No Action called ${type}`);
-  }
-}
-
-function Form() {
-  const [{ title, description }, dispatch] = useReducer(reducer, init);
-
-  function handleSubmit(evt) {
-    evt.preventDefault();
-    const formData = new FormData(evt.target);
-    const data = Object.fromEntries(formData.entries());
-    const db = getFirestore(app);
-    addDoc(collection(db, data.topic.toLowerCase()), {
-      title,
-      description,
-      date: serverTimestamp(),
-      url: data.file.name,
-    })
-      .then((ref) => console.log(ref))
-      .catch((error) => console.log(error));
-  }
-
-  return (
-    <form className={classes.form} onSubmit={handleSubmit}>
-      <RadioGroup />
-      <Input
-        value={title}
-        setValue={(value) => dispatch({ type: "title", payload: value })}
-        type="text"
-        placeholder="Title"
-      />
-      <Textarea
-        value={description}
-        setValue={(value) => dispatch({ type: "description", payload: value })}
-        placeholder="Say more about current News or Notice"
-      />
-      <FileInput />
-      <Input type="submit" value="Post" />
-    </form>
-  );
-}
-
-function RadioGroup() {
-  return (
-    <div className={classes.radioGroup}>
-      <RadioInput name="topic" value="News" defaultChecked={true} />
-      <RadioInput name="topic" value="Notice" />
-    </div>
   );
 }

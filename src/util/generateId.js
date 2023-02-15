f89a2224ae91184d9a5fa8e3b2299e3a7f519aac
al-imam <alimam01828@gmail.com>
2023-02-15 23:38:01 +0600

    add - after every 8 character

--- src/util/generateId.js
+++ src/util/generateId.js
@@ -1,6 +1,8 @@
 function generateId(len) {
   const arr = crypto.getRandomValues(new Uint8Array((len || 40) / 2));
-  return Array.from(arr, (dec) => dec.toString(16).padStart(2, "0")).join("");
+  return Array.from(arr, (dec) => dec.toString(16).padStart(2, "0"))
+    .join("")
+    .replace(/(.{8})/g, "$1-");
 }
 
 export default generateId;

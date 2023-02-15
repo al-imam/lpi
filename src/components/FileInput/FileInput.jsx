e2ec1fffaf2840f5b01741ac89497de012471d34
al-imam <alimam01828@gmail.com>
2023-02-15 10:47:08 +0600

    change logic for image selection

--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -1,17 +1,19 @@
 import classes from "./fileInput.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Trash } from "@svg";
 
 function FileInput() {
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState(null);
+  const ref = useRef(null);
 
   function remove() {
-    setFile("");
+    setFile(null);
+    ref.current.value = "";
   }
 
   return (
     <div className={classes.wrapper}>
-      {file ? (
+      {file && (
         <div className={classes.image}>
           <img src={URL.createObjectURL(file)} />
           <div className={classes.delete}>
@@ -20,16 +22,16 @@ function FileInput() {
             </button>
           </div>
         </div>
-      ) : (
-        <div className={classes.container}>
-          <input
-            onChange={(evt) => setFile(evt.target.files[0])}
-            type="file"
-            name="file"
-            className={classes.input}
-          />
-        </div>
       )}
+      <div className={classes.container}>
+        <input
+          ref={ref}
+          onChange={(evt) => setFile(evt.target.files[0])}
+          type="file"
+          name="file"
+          className={classes.input}
+        />
+      </div>
     </div>
   );
 }

42571d0b1d0e38cd628ea0c3609a8831619ef018
al-imam <alimam01828@gmail.com>
2023-02-17 14:17:16 +0600

    use useGetData to get firebase data

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,49 +1,27 @@
 import Post from "@components/Post/Post";
 import classes from "./news.module.css";
 import Location from "@components/Location/Location";
-
-import { useState, useEffect } from "react";
-
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import useGetData from "../../hooks/useGetData";
 
 function News() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function get() {
-      const db = getFirestore();
-      const cRef = collection(db, "news");
-      const querySnapshot = (await getDocs(cRef)).docs;
-      const post = querySnapshot.map((q) => ({ ...q.data(), id: q.id }));
-      console.log(post);
-      setData(post);
-    }
-    get();
-  }, []);
-
+  const { documents, error, loading } = useGetData("news");
   return (
     <>
       <Location current="News And event" />
       <section className={classes.section}>
         <div className={classes.container}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          {data.length > 0 &&
-            data.map((e) => (
-              <Post
-                id={e.id}
-                url={e.url}
-                date={JSON.stringify(
-                  new Date(e.date.seconds * 1000).toDateString()
-                )}
-                title={e.title}
-                description={e.description}
-              />
-            ))}
-          {JSON.stringify(data)}
+          {loading
+            ? "Loading..."
+            : documents.map((e) => (
+                <Post
+                  id={e.id}
+                  url={e.url}
+                  date={e.date.toDateString()}
+                  title={e.title}
+                  description={e.description}
+                />
+              ))}
+          {JSON.stringify(documents) + " " + JSON.stringify(error)}
         </div>
       </section>
     </>

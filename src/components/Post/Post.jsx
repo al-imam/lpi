4c926a0a0ace56b1b53e4301b8b310c1bfb06222
al-imam <alimam01828@gmail.com>
2023-02-17 12:50:02 +0600

    make component ready for reuse

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,29 +1,28 @@
 import classes from "./post.module.css";
 import { Download } from "@svg";
 
-function Post() {
+function Post({
+  title = "Lorem ipsum dolor sit amet",
+  description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptates praesentium enim. Velit veritatis quasi dicta, omnis eaque qui numquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit obcaecati accusantium recusandae aut asperiores magni dignissimos error consequatur expedita iusto?",
+  date = new Date().toDateString(),
+  url = "https://cbseguides.com/wp-content/uploads/2022/03/Notice-Writing.jpg",
+  id = null,
+}) {
   return (
-    <div className={classes.post}>
+    <div className={classes.post} id={id}>
       <p className={classes.title}>
-        <p className={classes.text}>Lorem ipsum dolor sit amet.</p>
-        <p className={classes.subtitle}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-          voluptates praesentium enim. Velit veritatis quasi dicta, omnis eaque
-          qui numquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Odit obcaecati accusantium recusandae aut asperiores magni dignissimos
-          error consequatur expedita iusto?
-        </p>
-        <time className={classes.time}>{new Date().toDateString()}</time>
+        <p className={classes.text}>{title}</p>
+        <p className={classes.subtitle}>{description}</p>
+        <time className={classes.time}>{date}</time>
       </p>
-      <div className={classes.image}>
-        <img
-          src="https://cbseguides.com/wp-content/uploads/2022/03/Notice-Writing.jpg"
-          alt="Post image"
-        />
-        <a href="/assets/lab.jpg" download={true}>
-          <Download />
-        </a>
-      </div>
+      {url !== null && (
+        <div className={classes.image}>
+          <img src={url} alt="Post image" />
+          <a href={url} download={true}>
+            <Download />
+          </a>
+        </div>
+      )}
     </div>
   );
 }

01e50c34657b875f45d346b9363dc8eb0c314c2d
al-imam <alimam01828@gmail.com>
2023-02-17 13:48:48 +0600

    fetch collection from firebase and store it

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -2,7 +2,25 @@ import Post from "@components/Post/Post";
 import classes from "./news.module.css";
 import Location from "@components/Location/Location";
 
+import { useState, useEffect } from "react";
+
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+
 function News() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function get() {
+      const db = getFirestore();
+      const cRef = collection(db, "news");
+      const querySnapshot = (await getDocs(cRef)).docs;
+      const post = querySnapshot.map((q) => ({ ...q.data(), id: q.id }));
+      console.log(post);
+      setData(post);
+    }
+    get();
+  }, []);
+
   return (
     <>
       <Location current="News And event" />
@@ -13,6 +31,19 @@ function News() {
           <Post />
           <Post />
           <Post />
+          {data.length > 0 &&
+            data.map((e) => (
+              <Post
+                id={e.id}
+                url={e.url}
+                date={JSON.stringify(
+                  new Date(e.date.seconds * 1000).toDateString()
+                )}
+                title={e.title}
+                description={e.description}
+              />
+            ))}
+          {JSON.stringify(data)}
         </div>
       </section>
     </>

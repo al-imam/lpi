090588bdcaacdc87fe0b82a675b0bc8b01397cc2
al-imam <alimam01828@gmail.com>
2023-02-17 11:01:09 +0600

    make alert working

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,14 +1,16 @@
 import classes from "./alert.module.css";
 import { SuccessIcon, ErrorIcon, CloseIcon } from "@svg";
 
-function Alter({ error = true, text = "Lorem ipsum dolor sit" }) {
+function Alter({ error = true, text = "Lorem ipsum dolor sit", close }) {
   return (
-    <div className={classes.alert + " " + classes.error}>
+    <div
+      className={`${classes.alert} ${error ? classes.error : classes.success}`}
+    >
       <div className={classes.svg}>
         {error ? <ErrorIcon /> : <SuccessIcon />}
       </div>
       <p className={classes.text}>{text}</p>
-      <button className={classes.button}>
+      <button onClick={close} type="button" className={classes.button}>
         <CloseIcon />
       </button>
     </div>

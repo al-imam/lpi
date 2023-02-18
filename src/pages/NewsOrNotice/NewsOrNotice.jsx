6b14799279f7d592a9fa30114bc37994c4725131
al-imam <alimam01828@gmail.com>
2023-02-18 12:12:26 +0600

    show error when response not come

--- src/pages/NewsOrNotice/NewsOrNotice.jsx
+++ src/pages/NewsOrNotice/NewsOrNotice.jsx
@@ -2,6 +2,7 @@ import Post from "@components/Post/Post";
 import classes from "./newsOrNotice.module.css";
 import Location from "@components/Location/Location";
 import useGetData from "@hooks/useGetData";
+import Alter from "../../components/Alert/Alert";
 
 const date = Intl.DateTimeFormat("bn-bd", {
   dateStyle: "full",
@@ -14,18 +15,20 @@ function NewsOrNotice({ location, collectionRef }) {
       <Location current={location} />
       <section className={classes.section}>
         <div className={classes.container}>
-          {loading && error === null
-            ? "Loading..."
-            : documents.map((e) => (
-                <Post
-                  key={e.id}
-                  id={e.id}
-                  url={e.url}
-                  date={date.format(e.date)}
-                  title={e.title}
-                  description={e.description}
-                />
-              ))}
+          {loading === false &&
+            error === null &&
+            documents.map((e) => (
+              <Post
+                key={e.id}
+                id={e.id}
+                url={e.url}
+                date={date.format(e.date)}
+                title={e.title}
+                description={e.description}
+              />
+            ))}
+          {loading && error === null && "Loading......"}
+          {error && error}
         </div>
       </section>
     </>

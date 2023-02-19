f2ebe87becb344811be7916e828f4c680fa13752
al-imam <alimam01828@gmail.com>
2023-02-19 15:47:20 +0600

    add fallback in links component

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -8,21 +8,29 @@ function Links({
   link = "#",
   text = "View all",
   page = false,
+  fallback = "No Notice Available",
 }) {
+  const array = [];
   return (
     <div className={classes.container + " " + classes[font]}>
       <h2 className={classes.heading}>{data.heading}</h2>
-      <ul className={classes.items}>
-        {data.links.map((item) => (
-          <li key={JSON.stringify(item)}>
-            <Link path={item.link} text={item.text} />
-          </li>
-        ))}
-      </ul>
-      {page && (
-        <NavLink to={link} className={classes.action}>
-          {text} <ArrowRight className={classes.svg} />
-        </NavLink>
+      {array.length > 0 ? (
+        <>
+          <ul className={classes.items}>
+            {array.map((item) => (
+              <li key={JSON.stringify(item)}>
+                <Link path={item.link} text={item.text} />
+              </li>
+            ))}
+          </ul>
+          {page && (
+            <NavLink to={link} className={classes.action}>
+              {text} <ArrowRight className={classes.svg} />
+            </NavLink>
+          )}{" "}
+        </>
+      ) : (
+        <span>{fallback}</span>
       )}
     </div>
   );

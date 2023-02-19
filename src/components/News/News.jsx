99746615d5c164bcf7b33e81d454315df980bfdf
al-imam <alimam01828@gmail.com>
2023-02-19 16:05:13 +0600

    update fallback text

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,7 +2,14 @@ import Links from "@components/Links/Links";
 import data from "./data";
 
 function News() {
-  return <Links text="More News" data={data} page={true} />;
+  return (
+    <Links
+      text="More News"
+      data={data}
+      page={true}
+      fallback="No news available"
+    />
+  );
 }
 
 export default News;
--- src/components/RecentNotice/RecentNotice.jsx
+++ src/components/RecentNotice/RecentNotice.jsx
@@ -2,7 +2,14 @@ import Links from "@components/Links/Links";
 import data from "./data";
 
 function RecentNotice() {
-  return <Links text="More Notice" data={data} page={true} />;
+  return (
+    <Links
+      text="More Notice"
+      data={data}
+      page={true}
+      fallback="No notice available"
+    />
+  );
 }
 
 export default RecentNotice;

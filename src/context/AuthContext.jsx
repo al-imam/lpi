b0e1510fd0523a788c1182c2c8269a95b3389666
al-imam <alimam01828@gmail.com>
2023-02-20 05:28:05 +0600

    create auth provider

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,52 @@
+import app from "@app/firebase";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const auth = getAuth(app);
+
+const AuthContext = createContext(null);
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUSer] = (useState < User) | (null > null);
+  const [loading, setLoading] = useState(true);
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUSer(user);
+      setLoading(false);
+    });
+
+    return unSubscribe;
+  }, []);
+
+  const value = {
+    login,
+    logOut,
+    currentUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};

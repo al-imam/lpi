026eeed6badb6b76b205fa529051d5694b583edc
al-imam <alimam01828@gmail.com>
2023-02-20 10:50:59 +0600

    add remove user icons

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -523,3 +523,20 @@ export function UserAdd(props) {
     </svg>
   );
 }
+
+export function UserRemove(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M5 3h6a3 3 0 0 1 3 3v4h-1V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-4h1v4a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3m3 9h11.25L16 8.75l.66-.75l4.5 4.5l-4.5 4.5l-.66-.75L19.25 13H8v-1Z"
+      ></path>
+    </svg>
+  );
+}

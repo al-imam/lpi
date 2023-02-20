9fd103df112b7d1fa4ce137f97841c01264473d2
al-imam <alimam01828@gmail.com>
2023-02-20 10:46:23 +0600

    remove text and add login logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,10 +7,12 @@ export default function Header({ setOpen, open }) {
   return (
     <header className={classes.main}>
       <div className={classes.header}>
-        <NavLink className={classes.item} to="/">
-          <img className={classes.img} src={logo} alt="Logo" />
-          Polytechnic
-        </NavLink>
+        <div className={classes.group}>
+          <NavLink className={classes.item} to="/">
+            <img className={classes.img} src={logo} alt="Logo" />
+          </NavLink>
+          <button>LogIn</button>
+        </div>
         <ul className={classes.items}>
           <li>
             <a href="mailto:alimam01828@gmail.com" className={classes.item}>

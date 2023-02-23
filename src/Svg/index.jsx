5f94145a546c8b11a1d0aae8b4132d3577d1c6f2
al-imam <alimam01828@gmail.com>
2023-02-23 17:32:43 +0600

    add closeMenu animated svg

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -579,8 +579,8 @@ export function InitialMenu(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
+      width="32"
+      height="32"
       viewBox="0 0 24 24"
       {...props}
     >
@@ -616,6 +616,53 @@ export function InitialMenu(props) {
             begin="0.4s"
             dur="0.2s"
             values="24;0"
+          ></animate>
+        </path>
+      </g>
+    </svg>
+  );
+}
+
+export function CloseMenu(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth="2"
+      >
+        <path d="M5 5L19 5">
+          <animate
+            fill="freeze"
+            attributeName="d"
+            begin="0.2s"
+            dur="0.4s"
+            values="M5 5L19 5;M5 5L19 19"
+          ></animate>
+        </path>
+        <path d="M5 12H19">
+          <animate
+            fill="freeze"
+            attributeName="d"
+            dur="0.4s"
+            values="M5 12H19;M12 12H12"
+          ></animate>
+          <set attributeName="opacity" begin="0.4s" to="0"></set>
+        </path>
+        <path d="M5 19L19 19">
+          <animate
+            fill="freeze"
+            attributeName="d"
+            begin="0.2s"
+            dur="0.4s"
+            values="M5 19L19 19;M5 19L19 5"
           ></animate>
         </path>
       </g>

6f74f5f35817cd0d2984484fcb448b47e9ac6916
al-imam <alimam01828@gmail.com>
2023-02-23 18:06:29 +0600

    remove initial menu svg

--- src/Svg/index.jsx
+++ src/Svg/index.jsx
@@ -575,54 +575,6 @@ export function Admin(props) {
   );
 }
 
-export function InitialMenu(props) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
-      viewBox="0 0 24 24"
-      {...props}
-    >
-      <g
-        fill="none"
-        stroke="currentColor"
-        strokeDasharray="24"
-        strokeDashoffset="24"
-        strokeLinecap="round"
-        strokeWidth="2"
-      >
-        <path d="M5 5H19">
-          <animate
-            fill="freeze"
-            attributeName="stroke-dashoffset"
-            dur="0.2s"
-            values="24;0"
-          ></animate>
-        </path>
-        <path d="M5 12H19">
-          <animate
-            fill="freeze"
-            attributeName="stroke-dashoffset"
-            begin="0.2s"
-            dur="0.2s"
-            values="24;0"
-          ></animate>
-        </path>
-        <path d="M5 19H19">
-          <animate
-            fill="freeze"
-            attributeName="stroke-dashoffset"
-            begin="0.4s"
-            dur="0.2s"
-            values="24;0"
-          ></animate>
-        </path>
-      </g>
-    </svg>
-  );
-}
-
 export function CloseMenu(props) {
   return (
     <svg

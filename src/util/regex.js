0cf25e5658da9f9d76abdf618f7d7d0b745c8542
al-imam <alimam01828@gmail.com>
2023-02-24 14:08:17 +0600

    create letter function

--- src/util/regex.js
+++ src/util/regex.js
@@ -2,6 +2,10 @@ function startWith(ex) {
   return `^${ex}`;
 }
 
+function letter({ lowerCase = true, upperCase = true }) {
+  return `[${lowerCase ? "a-z" : ""}${upperCase ? "A-Z" : ""}]`;
+}
+
 const email = new RegExp();
 
 export default email;

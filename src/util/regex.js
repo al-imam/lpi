fb834e904074c14031b8e803f0fd29a91adc5b2b
al-imam <alimam01828@gmail.com>
2023-02-24 14:11:08 +0600

    create word function

--- src/util/regex.js
+++ src/util/regex.js
@@ -6,6 +6,10 @@ function letter({ lowerCase = true, upperCase = true }) {
   return `[${lowerCase ? "a-z" : ""}${upperCase ? "A-Z" : ""}]`;
 }
 
+function word({ underscore = false }) {
+  return `[a-zA-Z9-0${underscore ? "_" : ""}]`;
+}
+
 const email = new RegExp();
 
 export default email;

44b1b129c9c24addd6548f47efc305b298d6221e
al-imam <alimam01828@gmail.com>
2023-03-01 09:38:15 +0600

    get auth details from authProvider

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,12 +1,15 @@
 import classes from "./admin.module.css";
 import Form from "./Form";
+import { useAuth } from "@context/AuthContext";
 
 export default function Contact() {
+  const { currentUser } = useAuth();
+
   return (
     <main className={classes.s}>
       <section className={classes.wrapper}>
         <div className={classes.title}>
-          <h4>Hey, dear admin!</h4>
+          <h4>Hey, dear</h4>
           <p>
             The process of creating a post involves selecting "notice" or "news"
             type, adding a title and content, and optionally uploading an image.

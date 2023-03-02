050dcf78791acef4b8779cc9408bf4f2a5075451
al-imam <alimam01828@gmail.com>
2023-03-02 15:34:24 +0600

    modify department group take title prop

--- src/components/DepartmentGroup/DepartmentGroup.jsx
+++ src/components/DepartmentGroup/DepartmentGroup.jsx
@@ -2,11 +2,13 @@ import classes from "./departmentGroup.module.css";
 import DepartmentCard from "./DepartmentCard/DepartmentCard";
 import data from "./data";
 
-function DepartmentGroup() {
+function DepartmentGroup({ haveTitle = true }) {
   return (
     <section className={classes.section}>
       <div className={classes.container}>
-        <h2 className={classes.departmentTitle}>Our Deparments</h2>
+        {haveTitle && (
+          <h2 className={classes.departmentTitle}>our department's</h2>
+        )}
         <div className={classes.cardsContainer}>
           {data.map((card, index) => (
             <DepartmentCard

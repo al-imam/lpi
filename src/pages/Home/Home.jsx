2d459ef99960d3dbeab7b827bc37b55c28ef4289
al-imam <alimam01828@gmail.com>
2023-03-03 06:55:59 +0600

    lazy home page components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,29 @@
 import { lazy, Suspense } from "react";
 import classes from "./home.module.css";
-
-import DepartmentGroup from "@components/DepartmentGroup/DepartmentGroup";
 import Loader from "@components/Loader/Loader";
 import Marquee from "@components/Marquee/Marquee";
-import NoticeAndNews from "@components/NoticeAndNews/NoticeAndNews";
-import Principal from "@components/Principal/Principal";
-import Subscribe from "@components/Subscribe/Subscribe";
-import data from "./data";
 import Hero from "./Hero/Hero";
+import data from "./data";
 
-const Gallery = lazy(() => import("@components/Gallery/Gallery"));
+const DepartmentGroup = lazy(function () {
+  return import("@components/DepartmentGroup/DepartmentGroup");
+});
+
+const NoticeAndNews = lazy(function () {
+  return import("@components/NoticeAndNews/NoticeAndNews");
+});
+
+const Subscribe = lazy(function () {
+  return import("@components/Subscribe/Subscribe");
+});
+
+const Principal = lazy(function () {
+  return import("@components/Principal/Principal");
+});
+
+const Gallery = lazy(function () {
+  return import("@components/Gallery/Gallery");
+});
 
 function Home() {
   return (
@@ -22,10 +35,18 @@ function Home() {
       />
       <Hero />
       <Marquee />
-      <NoticeAndNews />
-      <Principal />
-      <DepartmentGroup />
-      <Subscribe />
+      <Suspense fallback={<Loader />}>
+        <NoticeAndNews />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <Principal />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <DepartmentGroup />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <Subscribe />
+      </Suspense>
       <Suspense fallback={<Loader />}>
         <Gallery pictures={data.gallery} heading="Image gallery" />
       </Suspense>

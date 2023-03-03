994e968007dd96b51ab57627d146ce4d98e412de
al-imam <alimam01828@gmail.com>
2023-03-03 06:39:20 +0600

    Fix about page structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,8 @@
-import { lazy, Suspense } from "react";
 import Location from "@components/Location/Location";
 import AboutUs from "@components/AboutUs/AboutUs";
 import Status from "@components/Status/Status";
-import Loader from "@components/Loader/Loader";
 import data from "./data";
-
-const Gallery = lazy(() => import("@components/Gallery/Gallery"));
+import Gallery from "@components/Gallery/Gallery";
 
 function About() {
   return (
@@ -13,9 +10,7 @@ function About() {
       <Location current="About" />
       <AboutUs subtitle={data.about} />
       <Status />
-      <Suspense fallback={<Loader />}>
-        <Gallery pictures={data.gallery} heading="Image Gallery" />
-      </Suspense>
+      <Gallery pictures={data.gallery} heading="Image Gallery" />
     </main>
   );
 }
